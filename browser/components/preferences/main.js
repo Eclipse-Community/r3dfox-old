a451ad48626981bf1652d4a98ff25863d3f2ffde
K4sum1 <k4sum1@eclipse.cx>
2024-03-25 15:07:15 -0700

    Revert "Bug 1845057 - Remove aClaimAllTypes from setDefaultBrowser. r=nrishel,settings-reviewers"
    This reverts commit 3e8c44dd66ee35c9d20ac7827e4c857ab114c786.

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -1770,7 +1770,7 @@ var gMainPane = {
       setDefaultButton.disabled = true;
 
       try {
-        await shellSvc.setDefaultBrowser(false);
+        await shellSvc.setDefaultBrowser(true, false);
       } catch (ex) {
         console.error(ex);
         return;

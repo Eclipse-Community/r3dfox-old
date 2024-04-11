b8feb0552789c2d4348d871f35737ab7ea852c33
K4sum1 <k4sum1@eclipse.cx>
2024-04-11 18:53:15 -0500

    Return to DX11(.0)/IDXGIResource::GetSharedHandle()

--- widget/gtk/DMABufSurface.h
+++ widget/gtk/DMABufSurface.h
@@ -35,8 +35,7 @@ typedef void* EGLSyncKHR;
 namespace mozilla {
 namespace gfx {
 class DataSourceSurface;
-class FileHandleWrapper;
-}  // namespace gfx
+}
 namespace layers {
 class MemoryOrShmem;
 class SurfaceDescriptor;
@@ -207,7 +206,7 @@ class DMABufSurface {
   uint64_t mBufferModifiers[DMABUF_BUFFER_PLANES];
 
   int mBufferPlaneCount;
-  RefPtr<mozilla::gfx::FileHandleWrapper> mDmabufFds[DMABUF_BUFFER_PLANES];
+  int mDmabufFds[DMABUF_BUFFER_PLANES];
   int32_t mDrmFormats[DMABUF_BUFFER_PLANES];
   int32_t mStrides[DMABUF_BUFFER_PLANES];
   int32_t mOffsets[DMABUF_BUFFER_PLANES];
@@ -217,9 +216,9 @@ class DMABufSurface {
   void* mMappedRegionData[DMABUF_BUFFER_PLANES];
   uint32_t mMappedRegionStride[DMABUF_BUFFER_PLANES];
 
-  RefPtr<mozilla::gfx::FileHandleWrapper> mSyncFd;
+  int mSyncFd;
   EGLSyncKHR mSync;
-  RefPtr<mozilla::gfx::FileHandleWrapper> mSemaphoreFd;
+  int mSemaphoreFd;
   RefPtr<mozilla::gl::GLContext> mGL;
 
   int mGlobalRefCountFd;
@@ -239,7 +238,7 @@ class DMABufSurfaceRGBA final : public DMABufSurface {
       int aWidth, int aHeight);
 
   static already_AddRefed<DMABufSurface> CreateDMABufSurface(
-      RefPtr<mozilla::gfx::FileHandleWrapper>&& aFd,
+      mozilla::UniqueFileHandle&& aFd,
       const mozilla::webgpu::ffi::WGPUDMABufInfo& aDMABufInfo, int aWidth,
       int aHeight);
 
@@ -299,7 +298,7 @@ class DMABufSurfaceRGBA final : public DMABufSurface {
   bool Create(const mozilla::layers::SurfaceDescriptor& aDesc) override;
   bool Create(mozilla::gl::GLContext* aGLContext, const EGLImageKHR aEGLImage,
               int aWidth, int aHeight);
-  bool Create(RefPtr<mozilla::gfx::FileHandleWrapper>&& aFd,
+  bool Create(mozilla::UniqueFileHandle&& aFd,
               const mozilla::webgpu::ffi::WGPUDMABufInfo& aDMABufInfo,
               int aWidth, int aHeight);
 

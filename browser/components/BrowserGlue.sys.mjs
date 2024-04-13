4927820eedcaf6e7a693ae771fb95dd7e1cb689a
K4sum1 <k4sum1@eclipse.cx>
2024-04-13 00:48:10 -0500

    Mypal68 style portable mode

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -5720,6 +5720,11 @@ export var DefaultBrowserCheck = {
       isDefaultError = true;
     }
 
+    let pService = Cc["@mozilla.org/toolkit/profile-service;1"].getService(
+      Ci.nsIToolkitProfileService
+    );
+
+    if (pService.portable() !=1 ) {
     if (isDefault && isStartupCheck) {
       let now = Math.floor(Date.now() / 1000).toString();
       Services.prefs.setCharPref(
@@ -5775,6 +5780,7 @@ export var DefaultBrowserCheck = {
     }
 
     return willPrompt;
+  }
   },
 };
 

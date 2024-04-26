92d82d04792b40125431b3561183cd96f515d6a0
K4sum1 <k4sum1@eclipse.cx>
2024-04-26 10:14:12 -0500

    Extend JPEG XL detection/recognition from Waterfox

--- browser/components/BrowserContentHandler.sys.mjs
+++ browser/components/BrowserContentHandler.sys.mjs
@@ -1295,6 +1295,7 @@ function maybeRecordToHandleTelemetry(uri, isLaunch) {
       ".xhtml",
       ".svg",
       ".webp",
+      ".jxl",
     ]);
     if (registeredExtensions.has(extension)) {
       counter[extension].add(1);

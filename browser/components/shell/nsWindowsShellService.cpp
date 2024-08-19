c844f89006c495315bdc709f49d7086008a9d7df
K4sum1 <k4sum1@eclipse.cx>
2024-08-18 21:21:25 -0500

    Fix installer version crashing (thanks @newbie-461)
    These changes are by @newbie-461 and part of their pull request except I retard and rebased before merging, so I had to do the changes myself.

--- browser/components/shell/nsWindowsShellService.cpp
+++ browser/components/shell/nsWindowsShellService.cpp
@@ -1836,6 +1836,7 @@ static nsresult PinShortcutToTaskbarImpl(bool aCheckOnly,
     return NS_ERROR_FILE_NOT_FOUND;
   }
 
+  if (IsWin11OrLater()) {
   auto pinWithWin11TaskbarAPIResults =
       PinCurrentAppToTaskbarWin11(aCheckOnly, aAppUserModelId);
   switch (pinWithWin11TaskbarAPIResults.result) {
@@ -1857,6 +1858,7 @@ static nsresult PinShortcutToTaskbarImpl(bool aCheckOnly,
       // an error occurs or for when pinning is not allowed
       // with the Win 11 APIs.
       break;
+  }
   }
 
   return PinCurrentAppToTaskbarWin10(aCheckOnly, aAppUserModelId,

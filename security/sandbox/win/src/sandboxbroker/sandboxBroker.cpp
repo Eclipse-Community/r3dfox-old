32b22ef3ba58157abacb42ebd4cd0c9a085a9922
K4sum1 <k4sum1@eclipse.cx>
2024-10-28 09:52:41 -0500

    Vista workarounds to get it running

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -308,6 +308,9 @@ static void AddLLVMProfilePathDirectoryToPolicy(
 #undef WSTRING
 
 static void EnsureAppLockerAccess(sandbox::TargetPolicy* aPolicy) {
+  if (!IsWin7OrLater()) {
+    return;
+  }
   if (aPolicy->GetLockdownTokenLevel() < sandbox::USER_LIMITED) {
     // The following rules are to allow DLLs to be loaded when the token level
     // blocks access to AppLocker. If the sandbox does not allow access to the

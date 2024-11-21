475041a7e08ff557f836044f678633ef5d6c9ff8
K4sum1 <k4sum1@eclipse.cx>
2024-11-21 07:34:58 -0600

    Stop browser chrome from forcing the non-native theme

--- servo/components/style/properties/data.py
+++ servo/components/style/properties/data.py
@@ -573,7 +573,6 @@ class Longhand(Property):
                 "LineBreak",
                 "LineClamp",
                 "MasonryAutoFlow",
-                "MozTheme",
                 "BoolInteger",
                 "text::MozControlCharacterVisibility",
                 "MathDepth",

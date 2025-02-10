fb39dff8353c19433424e86b7e1e7daa1d286138
K4sum1 <k4sum1@eclipse.cx>
2025-02-10 02:29:23 -0600

    View Image context entry preference disabled by default
    Combined original commit and preference into one.
    
    Original description:
    At first I was probably going to drop this, but I've gotten used to it, and it's a lot better than the only able to open image in new tab in normal Firefox. Here you can view the image in the same tab, or middle click to open in a new tab in the background.

--- browser/base/content/nsContextMenu.sys.mjs
+++ browser/base/content/nsContextMenu.sys.mjs
@@ -741,7 +741,42 @@ export class nsContextMenu {
       !this.onAudio &&
       !this.onLink &&
       !this.onTextInput;
-    this.showItem("context-viewimage", showViewImage || showBGImage);
+    if (Services.prefs.getBoolPref("r3dfox.view.image")) {
+      this.showItem("context-viewimage", showViewImage);
+
+      var shouldShow = !(
+        this.isContentSelected ||
+        this.onImage ||
+        this.onCanvas ||
+        this.onVideo ||
+        this.onAudio ||
+        this.onLink ||
+        this.onTextInput
+      );
+
+      this.showItem(
+        "context-viewbgimage",
+        shouldShow &&
+          !this.hasMultipleBGImages &&
+          !this.inSyntheticDoc &&
+          !this.inPDFViewer
+      );
+
+      this.showItem(
+        "context-sep-viewbgimage",
+        shouldShow &&
+          !this.hasMultipleBGImages &&
+          !this.inSyntheticDoc &&
+          !this.inPDFViewer
+      );
+
+      this.document.getElementById("context-viewbgimage").disabled = !this.hasBGImage;
+      this.showItem("context-viewimagetab", false);
+    } else {
+      this.showItem("context-viewimagetab", showViewImage || showBGImage);
+      this.showItem("context-viewimage", false);
+      this.showItem("context-viewbgimage", false);
+    }
 
     // Save image depends on having loaded its content.
     this.showItem(
@@ -873,11 +908,21 @@ export class nsContextMenu {
     this.showItem("context-inspect-a11y", showInspectA11Y);
 
     // View video depends on not having a standalone video.
-    this.showItem(
-      "context-viewvideo",
-      this.onVideo && (!this.inSyntheticDoc || this.inFrame)
-    );
-    this.setItemAttr("context-viewvideo", "disabled", !this.mediaURL);
+    if (Services.prefs.getBoolPref("r3dfox.view.image")) {
+      this.showItem(
+        "context-viewvideo",
+        this.onVideo && (!this.inSyntheticDoc || this.inFrame)
+      );
+      this.setItemAttr("context-viewvideo", "disabled", !this.mediaURL);
+      this.showItem("context-viewvideotab", false);
+    } else {
+      this.showItem(
+        "context-viewvideotab",
+        this.onVideo && (!this.inSyntheticDoc || this.inFrame)
+      );
+      this.setItemAttr("context-viewvideotab", "disabled", !this.mediaURL);
+      this.showItem("context-viewvideo", false);
+    }
   }
 
   initMiscItems() {
@@ -1788,8 +1833,10 @@ export class nsContextMenu {
   // Change current window to the URL of the image, video, or audio.
   viewMedia(e) {
     let where = lazy.BrowserUtils.whereToOpenLink(e, false, false);
-    if (where == "current") {
-      where = "tab";
+    if (!Services.prefs.getBoolPref("r3dfox.view.image")) {
+      if (where == "current") {
+        where = "tab";
+      }
     }
     let referrerInfo = this.contentData.referrerInfo;
     let systemPrincipal = Services.scriptSecurityManager.getSystemPrincipal();

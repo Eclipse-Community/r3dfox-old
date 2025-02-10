e7beadb727563eca208020857497ee72bef48996
K4sum1 <k4sum1@eclipse.cx>
2025-02-10 03:22:22 -0600

    r3dfox theme and settings

--- browser/branding/official/pref/firefox-branding.js
+++ browser/branding/official/pref/firefox-branding.js
@@ -44,3 +44,7 @@ pref("app.update.badgeWaitTime", 345600);
 // Number of usages of the web console.
 // If this is less than 5, then pasting code into the web console is disabled
 pref("devtools.selfxss.count", 5);
+
+// Plasmafox customizations
+pref("r3dfox.browser.plasmafox", false);
+pref("r3dfox.colors", 1);

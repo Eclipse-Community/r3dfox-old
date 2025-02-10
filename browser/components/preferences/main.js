6301569b1cb73b84202fe95c48b0594fc85fe376
K4sum1 <k4sum1@eclipse.cx>
2025-02-10 03:22:22 -0600

    r3dfox theme and settings

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -61,6 +61,28 @@ Preferences.addAll([
   { id: "browser.download.folderList", type: "int" },
   { id: "browser.download.dir", type: "file" },
 
+  /* r3dfox preferences */
+
+  { id: "widget.non-native-theme.enabled", type: "bool",  inverted: true },
+  { id: "widget.native-controls.scrollbar-style", type: "int" },
+  { id: "widget.non-native-theme.scrollbar.style", type: "int" },
+  { id: "widget.native-controls.override-win-version", type: "int" },
+  { id: "r3dfox.colors", type: "int" },
+  { id: "r3dfox.customizations.enabled", type: "bool" },
+  { id: "r3dfox.drag-space.enabled", type: "bool" },
+  { id: "r3dfox.backgrounds.enabled", type: "bool" },
+  { id: "r3dfox.transparent.menubar", type: "bool" },
+  { id: "r3dfox.translucent.navbar", type: "bool" },
+  { id: "r3dfox.aero.fog", type: "int" },
+  { id: "r3dfox.caption.text.color", type: "int" },
+  { id: "browser.urlbar.oneOffsInstant", type: "bool" },
+  { id: "r3dfox.view.image", type: "bool" },
+  { id: "browser.e10s.disabled", type: "bool" },
+  { id: "security.csp.enable", type: "bool",  inverted: true },
+  { id: "network.stricttransportsecurity.enabled", type: "bool",  inverted: true },
+  { id: "accessibility.force_disabled", type: "int" },
+  { id: "browser.tabs.hoverPreview.enabled", type: "bool" },
+
   /* Tab preferences
   Preferences:
 
@@ -89,7 +111,6 @@ Preferences.addAll([
   { id: "browser.warnOnQuitShortcut", type: "bool" },
   { id: "browser.tabs.warnOnOpen", type: "bool" },
   { id: "browser.ctrlTab.sortByRecentlyUsed", type: "bool" },
-  { id: "browser.tabs.hoverPreview.enabled", type: "bool" },
   { id: "browser.tabs.hoverPreview.showThumbnails", type: "bool" },
 
   { id: "sidebar.verticalTabs", type: "bool" },

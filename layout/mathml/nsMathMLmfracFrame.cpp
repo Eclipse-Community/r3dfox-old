b0311ce745df0205065168a8229af832a2e716c5
K4sum1 <k4sum1@eclipse.cx>
2025-02-18 00:03:40 -0600

    Revert "Bug 1916988 - Set vertical offset of mfrac denominator from the baseline. r=emilio"
    This reverts commit 859cd830958b2d68a3913385d2af541996a8c41b.

--- layout/mathml/nsMathMLmfracFrame.cpp
+++ layout/mathml/nsMathMLmfracFrame.cpp
@@ -377,8 +377,8 @@ nsresult nsMathMLmfracFrame::Place(DrawTarget* aDrawTarget,
                       ReflowChildFlags::Default);
     // place denominator
     dxDen += denMargin.left;
-    dy =
-        aDesiredSize.BlockStartAscent() + denShift - sizeDen.BlockStartAscent();
+    dy = aDesiredSize.Height() - sizeDen.Height() - denMargin.bottom -
+         borderPadding.bottom;
     FinishReflowChild(frameDen, presContext, sizeDen, nullptr, dxDen, dy,
                       ReflowChildFlags::Default);
     // place the fraction bar - dy is top of bar

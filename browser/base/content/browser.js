ec5fe4980b5d375c3aafad67cb3613568b9f2ea8
K4sum1 <k4sum1@eclipse.cx>
2025-03-03 00:47:28 -0600

    Revert "Bug 1932600 - Use customtitlebar rather than tabsintitlebar in front-end code. r=dao,desktop-theme-reviewers,tabbrowser-reviewers,sidebar-reviewers,nsharpley"
    This reverts commit 06b1c50bed2bd119544d28d5896d2354693bba81.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6670,7 +6670,7 @@ var ToolbarIconColor = {
   _windowState: {
     active: false,
     fullscreen: false,
-    customtitlebar: false,
+    tabsintitlebar: false,
   },
   init() {
     this._initialized = true;
@@ -6737,8 +6737,8 @@ var ToolbarIconColor = {
       case "toolbarvisibilitychange":
         // toolbar changes dont require reset of the cached color values
         break;
-      case "customtitlebar":
-        this._windowState.customtitlebar = reasonValue;
+      case "tabsintitlebar":
+        this._windowState.tabsintitlebar = reasonValue;
         break;
     }
 

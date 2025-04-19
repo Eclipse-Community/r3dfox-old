5f147278ce350cca83359ce2856cee1061fe28c3
K4sum1 <k4sum1@eclipse.cx>
2025-04-19 15:25:24 -0500

    Revert "Bug 1944998 - Explicitly opt in per window to mica backdrop. r=desktop-theme-reviewers,dao"
    This reverts commit 449913f473f47b7e71e4dead2b99aa530228dfda.

--- xpcom/ds/StaticAtoms.py
+++ xpcom/ds/StaticAtoms.py
@@ -1617,7 +1617,6 @@ STATIC_ATOMS = [
     Atom("viewBox", "viewBox"),
     Atom("viewTarget", "viewTarget"),
     Atom("white_space", "white-space"),
-    Atom("windowsmica", "windowsmica"),
     Atom("word_spacing", "word-spacing"),
     Atom("writing_mode", "writing-mode"),
     Atom("x", "x"),

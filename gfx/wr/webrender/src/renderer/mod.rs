810eed480ee76cf7506717a7d1b0265e618879f6
K4sum1 <k4sum1@eclipse.cx>
2025-04-19 11:39:24 -0500

    Fix hidden caption buttons with theme applied
    https://github.com/NetworkNeighborhood/Marble/commit/971fdfe1d2126e22d469f1efa1dfb6246bdaa6b8

--- gfx/wr/webrender/src/renderer/mod.rs
+++ gfx/wr/webrender/src/renderer/mod.rs
@@ -3391,12 +3391,13 @@ impl Renderer {
             self.gpu_profiler.finish_sampler(opaque_sampler);
         }
 
-        if !clear_tiles.is_empty() {
+        // Draw alpha tiles
+        if !occlusion.alpha_items().is_empty() {
             let transparent_sampler = self.gpu_profiler.start_sampler(GPU_SAMPLER_TAG_TRANSPARENT);
             self.set_blend(true, FramebufferKind::Main);
-            self.device.set_blend_mode_premultiplied_dest_out();
+            self.set_blend_mode_premultiplied_alpha(FramebufferKind::Main);
             self.draw_tile_list(
-                clear_tiles.iter(),
+                occlusion.alpha_items().iter().rev(),
                 &composite_state,
                 &composite_state.external_surfaces,
                 projection,
@@ -3405,13 +3406,12 @@ impl Renderer {
             self.gpu_profiler.finish_sampler(transparent_sampler);
         }
 
-        // Draw alpha tiles
-        if !occlusion.alpha_items().is_empty() {
+        if !clear_tiles.is_empty() {
             let transparent_sampler = self.gpu_profiler.start_sampler(GPU_SAMPLER_TAG_TRANSPARENT);
             self.set_blend(true, FramebufferKind::Main);
-            self.set_blend_mode_premultiplied_alpha(FramebufferKind::Main);
+            self.device.set_blend_mode_premultiplied_dest_out();
             self.draw_tile_list(
-                occlusion.alpha_items().iter().rev(),
+                clear_tiles.iter(),
                 &composite_state,
                 &composite_state.external_surfaces,
                 projection,
@@ -3450,7 +3450,7 @@ impl Renderer {
 
         for (idx, tile) in composite_state.tiles.iter().enumerate() {
             // Clear tiles overwrite whatever is under them, so they are treated as opaque.
-            let is_opaque = tile.kind != TileKind::Alpha;
+            let is_opaque = tile.kind == TileKind::Opaque;
 
             let device_tile_box = composite_state.get_device_rect(
                 &tile.local_rect,
@@ -3477,6 +3477,8 @@ impl Renderer {
                 continue;
             }
 
+            occlusion.add(&rect, is_opaque, idx);
+
             if tile.kind == TileKind::Clear {
                 // Clear tiles are specific to how we render the window buttons on
                 // Windows 8. They clobber what's under them so they can be treated as opaque,
@@ -3485,8 +3487,6 @@ impl Renderer {
                 clear_tiles.push(occlusion::Item { rectangle: rect, key: idx });
                 continue;
             }
-
-            occlusion.add(&rect, is_opaque, idx);
         }
 
         // If experimental compositor is enabled, notify it that we are beginning
@@ -4529,9 +4529,9 @@ impl Renderer {
             // Invalidate any native surface tiles that might be updated by passes.
             if !frame.has_been_rendered {
                 for tile in &frame.composite_state.tiles {
-                    if tile.kind == TileKind::Clear {
-                        continue;
-                    }
+                    //if tile.kind == TileKind::Clear {
+                    //    continue;
+                    //}
                     if !tile.local_dirty_rect.is_empty() {
                         if let CompositeTileSurface::Texture { surface: ResolvedSurfaceTexture::Native { id, .. } } = tile.surface {
                             let valid_rect = frame.composite_state.get_surface_rect(

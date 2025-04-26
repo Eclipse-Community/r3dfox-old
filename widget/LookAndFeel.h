28a84880208a129998f0cb436ca46440cc6a0dd8
K4sum1 <k4sum1@eclipse.cx>
2025-04-25 19:32:06 -0500

    Fix broken tooltips on 32 bit build
    Reverts https://github.com/Eclipse-Community/r3dfox/commit/ade0599fdfacddb5af57e04e36980c021093343d
    
    Could be better, but I don't know how to properly fix the issue.

--- widget/LookAndFeel.h
+++ widget/LookAndFeel.h
@@ -229,7 +229,6 @@ class LookAndFeel {
      */
     ContextMenuOffsetVertical,
     ContextMenuOffsetHorizontal,
-    TooltipOffsetVertical,
 
     /*
      * A boolean value indicating whether client-side decorations are
@@ -378,11 +377,6 @@ class LookAndFeel {
   // This is a common enough integer that seems worth the shortcut.
   static bool UseOverlayScrollbars() {
     return GetInt(IntID::UseOverlayScrollbars);
-  }
-
-  static constexpr int32_t kDefaultTooltipOffset = 21;
-  static int32_t TooltipOffsetVertical() {
-    return GetInt(IntID::TooltipOffsetVertical, kDefaultTooltipOffset);
   }
 
   // Returns keyCode value of a modifier key which is used for accesskey.
--- widget/windows/nsLookAndFeel.cpp
+++ widget/windows/nsLookAndFeel.cpp
@@ -37,17 +37,6 @@ static int32_t GetSystemParam(long flag, int32_t def) {
   return ::SystemParametersInfo(flag, 0, &value, 0) ? value : def;
 }
 
-static int32_t GetTooltipOffsetVertical() {
-  static constexpr DWORD kDefaultCursorSize = 32;
-  const DWORD cursorSize =
-      GetSystemParam(MOZ_SPI_CURSORSIZE, kDefaultCursorSize);
-  if (cursorSize == kDefaultCursorSize) {
-    return LookAndFeel::kDefaultTooltipOffset;
-  }
-  return std::ceilf(float(LookAndFeel::kDefaultTooltipOffset) *
-                    float(cursorSize) / float(kDefaultCursorSize));
-}
-
 static bool SystemWantsDarkTheme() {
   if (!IsWin10OrLater()) {
     return false;
@@ -587,9 +576,6 @@ nsresult nsLookAndFeel::NativeGetInt(IntID aID, int32_t& aResult) {
     case IntID::ContextMenuOffsetVertical:
     case IntID::ContextMenuOffsetHorizontal:
       aResult = 2;
-      break;
-    case IntID::TooltipOffsetVertical:
-      aResult = GetTooltipOffsetVertical();
       break;
     case IntID::SystemUsesDarkTheme:
       aResult = SystemWantsDarkTheme();

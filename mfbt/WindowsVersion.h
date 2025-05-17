1efaaf562cf9e8a022270b8671cd7ce33d3c1e32
K4sum1 <k4sum1@eclipse.cx>
2025-05-17 16:04:24 -0500

    Fix font on Vista? (Maybe set this to IsWinVistaOrLater instead?)

--- mfbt/WindowsVersion.h
+++ mfbt/WindowsVersion.h
@@ -167,7 +167,7 @@ MOZ_ALWAYS_INLINE bool IsWin11OrLater() {
 }
 
 MOZ_ALWAYS_INLINE bool IsNotWin7PreRTM() {
-  return IsWin7SP1OrLater() || IsWindowsBuildOrLater(7600);
+  return IsWin7SP1OrLater() || IsWindowsBuildOrLater(2000);
 }
 
 inline bool IsWin7AndPre2000Compatible() {

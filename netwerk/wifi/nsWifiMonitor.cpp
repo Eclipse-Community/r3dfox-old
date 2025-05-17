379f27c0c8dfa6dc1e17c7c428a44030ce6a2a72
K4sum1 <k4sum1@eclipse.cx>
2025-05-17 13:36:52 -0500

    Bug 1900225: Part 6 - Wait for Windows permission dialog to be dismissed before requesting geolocation r=emilio,win-reviewers,gstoll    2024-08-28 06:47:33    48c6934e1dc7e58493b8142e35029a08d2bf4f23 Bug 1900225: Part 5 - On Windows, do not show message about wifi scan permission if no wifi is present r=win-reviewers,necko-reviewers,valentin,gstoll    2024-08-28 06:47:33    cf53d2cd9f2325321fdaf212a37e13ed8e6ebbaf Bug 1900225: Part 4 - Add geo.prompt.open_system_prefs to block opening system preferences r=gstoll    2024-08-28 06:47:32    5a9e60b4decd7b81c9d561c574520ab0a83e4709 Bug 1900225: Part 3 - Implement system geolocation permission UX on Windows r=win-reviewers,gstoll    2024-08-28 06:47:32    cacd3ce9eb7aff7f1046c0c2c031a9df30c53b0f

--- netwerk/wifi/nsWifiMonitor.cpp
+++ netwerk/wifi/nsWifiMonitor.cpp
@@ -174,15 +174,6 @@ nsWifiMonitor::Observe(nsISupports* subject, const char* topic,
   return NS_OK;
 }
 
-void nsWifiMonitor::EnsureWifiScanner() {
-  if (mWifiScanner) {
-    return;
-  }
-
-  LOG(("Constructing WifiScanner"));
-  mWifiScanner = MakeUnique<mozilla::WifiScannerImpl>();
-}
-
 NS_IMETHODIMP nsWifiMonitor::StartWatching(nsIWifiListener* aListener,
                                            bool aForcePolling) {
   LOG(("nsWifiMonitor::StartWatching %p | listener %p | mPollingId %" PRIu64
@@ -323,7 +314,10 @@ void nsWifiMonitor::Scan(uint64_t aPollingId) {
 nsresult nsWifiMonitor::DoScan() {
   MOZ_ASSERT(IsBackgroundThread());
 
-  EnsureWifiScanner();
+  if (!mWifiScanner) {
+    LOG(("Constructing WifiScanner"));
+    mWifiScanner = MakeUnique<mozilla::WifiScannerImpl>();
+  }
   MOZ_ASSERT(mWifiScanner);
 
   LOG(("Scanning Wifi for access points"));
@@ -409,16 +403,4 @@ nsresult nsWifiMonitor::PassErrorToWifiListeners(nsresult rv) {
   return NotifyListeners([&](nsIWifiListener* aListener, WifiListenerData&) {
     aListener->OnError(rv);
   });
-}
-
-bool nsWifiMonitor::GetHasWifiAdapter() {
-#ifdef XP_WIN
-  EnsureWifiScanner();
-  MOZ_ASSERT(mWifiScanner);
-  return static_cast<WifiScannerImpl*>(mWifiScanner.get())->HasWifiAdapter();
-#else
-  MOZ_ASSERT_UNREACHABLE(
-      "nsWifiMonitor::HasWifiAdapter is not available on this platform");
-  return false;
-#endif
 }

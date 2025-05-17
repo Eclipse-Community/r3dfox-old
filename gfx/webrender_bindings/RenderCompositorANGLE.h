2aa0a972a32ef74796bfb6d01c16ea114d6059ce
K4sum1 <k4sum1@eclipse.cx>
2025-05-17 13:35:58 -0500

    Bug 1926862 - Add transparency support to RenderCompositorANGLE. r=gfx-reviewers,nical    2024-11-09 06:17:11    7e6a74492cd34cfba9449fda5ffb5ed4c023d6b7

--- gfx/webrender_bindings/RenderCompositorANGLE.h
+++ gfx/webrender_bindings/RenderCompositorANGLE.h
@@ -32,7 +32,7 @@ namespace wr {
 
 class DCLayerTree;
 
-class RenderCompositorANGLE final : public RenderCompositor {
+class RenderCompositorANGLE : public RenderCompositor {
  public:
   static UniquePtr<RenderCompositor> Create(
       const RefPtr<widget::CompositorWidget>& aWidget, nsACString& aError);
@@ -112,8 +112,7 @@ class RenderCompositorANGLE final : public RenderCompositor {
                      bool* aNeedsYFlip) override;
 
  protected:
-  bool UseCompositor() const;
-  bool RecreateNonNativeCompositorSwapChain();
+  bool UseCompositor();
   void InitializeUsePartialPresent();
   void InsertGraphicsCommandsFinishedWaitQuery(
       RenderedFrameId aRenderedFrameId);
@@ -129,7 +128,6 @@ class RenderCompositorANGLE final : public RenderCompositor {
   RefPtr<ID3D11Query> GetD3D11Query();
   void ReleaseNativeCompositorResources();
   HWND GetCompositorHwnd();
-  bool ShouldUseAlpha() const;
 
   RefPtr<IDXGIDevice> DXGIDevice();
   RefPtr<IDXGIFactory> DXGIFactory();
@@ -160,8 +158,6 @@ class RenderCompositorANGLE final : public RenderCompositor {
   // Used to know a timing of disabling native compositor.
   bool mDisablingNativeCompositor = false;
   bool mFirstPresent = true;
-  // Wether we're currently using alpha.
-  bool mSwapChainUsingAlpha = false;
 };
 
 }  // namespace wr

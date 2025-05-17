eebf079c834269cecd59a380013a9c8fb2c08330
K4sum1 <k4sum1@eclipse.cx>
2025-05-17 13:42:13 -0500

    Bug 1844241 - Remove now-dead graphics glass code. r=tnikkel,devtools-reviewers    2023-07-19 20:44:34    9aa60b822ab33dae826dbd510aa3f3099232f51d

--- gfx/webrender_bindings/RenderCompositorANGLE.h
+++ gfx/webrender_bindings/RenderCompositorANGLE.h
@@ -124,7 +124,8 @@ class RenderCompositorANGLE : public RenderCompositor {
   bool CreateSwapChain(nsACString& aError);
   void CreateSwapChainForDCompIfPossible();
   bool CreateSwapChainForHWND();
-  RefPtr<IDXGISwapChain1> CreateSwapChainForDComp(bool aUseTripleBuffering);
+  RefPtr<IDXGISwapChain1> CreateSwapChainForDComp(bool aUseTripleBuffering,
+                                                  bool aUseAlpha);
   RefPtr<ID3D11Query> GetD3D11Query();
   void ReleaseNativeCompositorResources();
   HWND GetCompositorHwnd();
@@ -134,10 +135,11 @@ class RenderCompositorANGLE : public RenderCompositor {
 
   RefPtr<gl::GLContext> mGL;
 
-  EGLConfig mEGLConfig = nullptr;
-  EGLSurface mEGLSurface = nullptr;
+  EGLConfig mEGLConfig;
+  EGLSurface mEGLSurface;
 
-  bool mUseTripleBuffering = false;
+  bool mUseTripleBuffering;
+  bool mUseAlpha;
 
   RefPtr<ID3D11Device> mDevice;
   RefPtr<ID3D11DeviceContext> mCtx;
@@ -152,11 +154,11 @@ class RenderCompositorANGLE : public RenderCompositor {
   RenderedFrameId mLastCompletedFrameId;
 
   Maybe<LayoutDeviceIntSize> mBufferSize;
-  bool mUseNativeCompositor = true;
-  bool mUsePartialPresent = false;
-  bool mFullRender = false;
+  bool mUseNativeCompositor;
+  bool mUsePartialPresent;
+  bool mFullRender;
   // Used to know a timing of disabling native compositor.
-  bool mDisablingNativeCompositor = false;
+  bool mDisablingNativeCompositor;
   bool mFirstPresent = true;
 };
 

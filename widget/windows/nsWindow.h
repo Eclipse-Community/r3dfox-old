645f9a3a47c3a5aaff34e0d1ee1b02457a568e9c
K4sum1 <k4sum1@eclipse.cx>
2025-05-17 13:37:54 -0500

    Bug 1891120 - Remove GetWindowInfo hook. r=win-reviewers,rkraesig    2024-04-18 10:07:00    6d6930d179ffae21187306d3f79bab000799dde7

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -506,6 +506,7 @@ class nsWindow final : public nsBaseWidget {
   bool CanTakeFocus();
   bool UpdateNonClientMargins(bool aReflowWindow = true);
   void UpdateDarkModeToolbar();
+  void UpdateGetWindowInfoCaptionStatus(bool aActiveCaption);
   void ResetLayout();
   void InvalidateNonClientRegion();
   static const wchar_t* GetMainWindowClass();

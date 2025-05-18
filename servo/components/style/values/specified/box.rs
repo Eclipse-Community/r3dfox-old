0454ab65c242e7493279239e549da42db7c6f80d
K4sum1 <k4sum1@eclipse.cx>
2025-05-18 07:16:13 -0500

    Other stuff missing from r3dfox

--- servo/components/style/values/specified/box.rs
+++ servo/components/style/values/specified/box.rs
@@ -1669,9 +1669,6 @@ pub enum Appearance {
     /// The dropdown portion of a toolbar button
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     ToolbarbuttonDropdown,
-    /// The gripper for a toolbar.
-    #[parse(condition = "ParserContext::chrome_rules_enabled")]
-    Toolbargripper,
     /// The toolbox that contains the toolbars.
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     Toolbox,
@@ -1684,9 +1681,6 @@ pub enum Appearance {
     /// An individual header cell
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     Treeheadercell,
-    /// The sort arrow for a header.
-    #[parse(condition = "ParserContext::chrome_rules_enabled")]
-    Treeheadersortarrow,
     /// A tree item.
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     Treeitem,
--- widget/windows/nsNativeThemeWin.cpp
+++ widget/windows/nsNativeThemeWin.cpp
@@ -769,7 +769,6 @@ mozilla::Maybe<nsUXThemeClass> nsNativeThemeWin::GetThemeClass(
     case StyleAppearance::MozMenulistArrowButton:
       return Some(eUXCombobox);
     case StyleAppearance::Treeheadercell:
-    case StyleAppearance::Treeheadersortarrow:
       return Some(eUXHeader);
     case StyleAppearance::Listbox:
     case StyleAppearance::Treeview:
@@ -1215,12 +1214,6 @@ nsresult nsNativeThemeWin::GetThemePartAndState(nsIFrame* aFrame,
       } else
         aState = StandardGetState(aFrame, aAppearance, true);
 
-      return NS_OK;
-    }
-    case StyleAppearance::Treeheadersortarrow: {
-      // XXX Probably will never work due to a bug in the Luna theme.
-      aPart = 4;
-      aState = 1;
       return NS_OK;
     }
     case StyleAppearance::Treeheadercell: {

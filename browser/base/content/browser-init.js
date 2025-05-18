42a981250bf1d8803ed3bda7916d8439545546a1
K4sum1 <k4sum1@eclipse.cx>
2025-05-18 04:35:18 -0500

    To look at, I don't think this is needed so it's not in r3dfox

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -491,9 +491,7 @@ var gBrowserInit = {
     }
 
     FullScreen.init();
-    if (AppConstants.isPlatformAndVersionAtLeast("win", "10")) {
     MenuTouchModeObserver.init();
-    }
 
     if (AppConstants.MOZ_DATA_REPORTING) {
       gDataNotificationInfoBar.init();
@@ -1135,9 +1133,7 @@ var gBrowserInit = {
       );
       Services.obs.removeObserver(gKeywordURIFixup, "keyword-uri-fixup");
 
-    if (AppConstants.isPlatformAndVersionAtLeast("win", "10")) {
       MenuTouchModeObserver.uninit();
-    }
       BrowserOffline.uninit();
       CanvasPermissionPromptHelper.uninit();
       WebAuthnPromptHelper.uninit();

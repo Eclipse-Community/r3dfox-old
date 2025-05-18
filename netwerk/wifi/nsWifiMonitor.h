82c669f0d2c085e23cff061e5ff96bc70abed26e
K4sum1 <k4sum1@eclipse.cx>
2025-05-18 04:18:21 -0500

    Not sure what this Wifi stuff is for either

--- netwerk/wifi/nsWifiMonitor.h
+++ netwerk/wifi/nsWifiMonitor.h
@@ -61,6 +61,8 @@ class nsWifiMonitor final : public nsIWifiMonitor, public nsIObserver {
 
   ~nsWifiMonitor();
 
+  void EnsureWifiScanner();
+
   nsresult DispatchScanToBackgroundThread(uint64_t aPollingId = 0,
                                           uint32_t aWaitMs = 0);
 

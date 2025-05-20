2ef6cd7be58e6969d1b94cb360aa4513170af731
K4sum1 <k4sum1@eclipse.cx>
2025-05-20 15:18:34 -0500

    My mangling of the css for modern mode

--- servo/components/style/gecko/media_features.rs
+++ servo/components/style/gecko/media_features.rs
@@ -1002,6 +1002,7 @@ pub static MEDIA_FEATURES: [QueryFeatureDescription; 68] = [
     lnf_int_feature!(atom!("-moz-windows-compositor"), DWMCompositor),
     lnf_int_feature!(atom!("-moz-windows-classic"), WindowsClassic),
     lnf_int_feature!(atom!("-moz-windows-glass"), WindowsGlass),
+    lnf_int_feature!(atom!("-moz-windows-modern"), WindowsModern),
     lnf_int_feature!(atom!("-moz-swipe-animation-enabled"), SwipeAnimationEnabled),
     lnf_int_feature!(atom!("-moz-gtk-csd-available"), GTKCSDAvailable),
     lnf_int_feature!(atom!("-moz-gtk-csd-transparency-available"), GTKCSDTransparencyAvailable),

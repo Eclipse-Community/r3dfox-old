46fa092f81af2d37f855a8c85586a393e87dcd01
K4sum1 <k4sum1@eclipse.cx>
2025-05-19 20:47:06 -0500

    tramsparency part 1

--- widget/windows/InProcessWinCompositorWidget.cpp
+++ widget/windows/InProcessWinCompositorWidget.cpp
@@ -77,10 +77,6 @@ bool InProcessWinCompositorWidget::OnWindowResize(
   return true;
 }
 
-bool InProcessWinCompositorWidget::DrawsToMemoryDC() const {
-  return ::GetWindowLongPtrW(mWnd, GWL_EXSTYLE) & WS_EX_LAYERED;
-}
-
 void InProcessWinCompositorWidget::OnWindowModeChange(nsSizeMode aSizeMode) {}
 
 bool InProcessWinCompositorWidget::PreRender(WidgetRenderingContext* aContext) {
@@ -112,7 +108,7 @@ InProcessWinCompositorWidget::StartRemoteDrawing() {
   MOZ_ASSERT(!mCompositeDC);
 
   RefPtr<gfxASurface> surf;
-  if (DrawsToMemoryDC()) {
+  if (TransparencyModeIs(TransparencyMode::Transparent)) {
     surf = EnsureTransparentSurface();
   }
 
@@ -152,7 +148,7 @@ InProcessWinCompositorWidget::StartRemoteDrawing() {
 void InProcessWinCompositorWidget::EndRemoteDrawing() {
   MOZ_ASSERT(!mLockedBackBufferData);
 
-  if (DrawsToMemoryDC()) {
+  if (TransparencyModeIs(TransparencyMode::Transparent)) {
     MOZ_ASSERT(mTransparentSurface);
     RedrawTransparentWindow();
   }
@@ -241,7 +237,7 @@ void InProcessWinCompositorWidget::LeavePresentLock() { mPresentLock.Leave(); }
 
 RefPtr<gfxASurface> InProcessWinCompositorWidget::EnsureTransparentSurface() {
   mTransparentSurfaceLock.AssertCurrentThreadOwns();
-  MOZ_ASSERT(DrawsToMemoryDC());
+  MOZ_ASSERT(TransparencyModeIs(TransparencyMode::Transparent));
 
   IntSize size = GetClientSize().ToUnknownSize();
   if (!mTransparentSurface || mTransparentSurface->GetSize() != size) {
@@ -275,7 +271,7 @@ void InProcessWinCompositorWidget::UpdateTransparency(TransparencyMode aMode) {
   mTransparentSurface = nullptr;
   mMemoryDC = nullptr;
 
-  if (DrawsToMemoryDC()) {
+  if (aMode == TransparencyMode::Transparent) {
     EnsureTransparentSurface();
   }
 }
@@ -324,7 +320,7 @@ void InProcessWinCompositorWidget::ClearTransparentWindow() {
 }
 
 bool InProcessWinCompositorWidget::RedrawTransparentWindow() {
-  MOZ_ASSERT(DrawsToMemoryDC());
+  MOZ_ASSERT(TransparencyModeIs(TransparencyMode::Transparent));
 
   LayoutDeviceIntSize size = GetClientSize();
 
@@ -343,11 +339,12 @@ bool InProcessWinCompositorWidget::RedrawTransparentWindow() {
 }
 
 HDC InProcessWinCompositorWidget::GetWindowSurface() {
-  return DrawsToMemoryDC() ? mMemoryDC : ::GetDC(mWnd);
+  return TransparencyModeIs(TransparencyMode::Transparent) ? mMemoryDC
+                                                           : ::GetDC(mWnd);
 }
 
 void InProcessWinCompositorWidget::FreeWindowSurface(HDC dc) {
-  if (!DrawsToMemoryDC()) {
+  if (!TransparencyModeIs(TransparencyMode::Transparent)) {
     ::ReleaseDC(mWnd, dc);
   }
 }
--- widget/windows/nsWindowGfx.cpp
+++ widget/windows/nsWindowGfx.cpp
@@ -137,7 +137,7 @@ void nsWindow::ForcePresent() {
   }
 }
 
-bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
+bool nsWindow::OnPaint(uint32_t aNestingLevel) {
   gfx::DeviceResetReason resetReason = gfx::DeviceResetReason::OK;
   if (gfxWindowsPlatform::GetPlatform()->DidRenderingDeviceReset(
           &resetReason)) {
@@ -219,12 +219,16 @@ bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
   }
   mLastPaintBounds = mBounds;
 
-  if (!aDC && IsPopup() && renderer->GetBackendType() == LayersBackend::LAYERS_NONE &&
-      TransparencyMode::Transparent == mTransparencyMode) {
-    // For layered translucent windows all drawing should go to memory DC and no
-    // WM_PAINT messages are normally generated. To support asynchronous
-    // painting we force generation of WM_PAINT messages by invalidating window
-    // areas with RedrawWindow, InvalidateRect or InvalidateRgn function calls.
+  // For layered translucent windows all drawing should go to memory DC and no
+  // WM_PAINT messages are normally generated. To support asynchronous painting
+  // we force generation of WM_PAINT messages by invalidating window areas with
+  // RedrawWindow, InvalidateRect or InvalidateRgn function calls.
+  const bool usingMemoryDC =
+      renderer->GetBackendType() == LayersBackend::LAYERS_NONE &&
+      mTransparencyMode == TransparencyMode::Transparent;
+
+  HDC hDC = nullptr;
+  if (usingMemoryDC) {
     // BeginPaint/EndPaint must be called to make Windows think that invalid
     // area is painted. Otherwise it will continue sending the same message
     // endlessly.
@@ -233,13 +237,13 @@ bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
 
     // We're guaranteed to have a widget proxy since we called
     // GetLayerManager().
-    aDC = mBasicLayersSurface->GetTransparentDC();
+    hDC = mBasicLayersSurface->GetTransparentDC();
+  } else {
+    hDC = ::BeginPaint(mWnd, &ps);
   }
 
-  HDC hDC = aDC ? aDC : ::BeginPaint(mWnd, &ps);
-
-  bool forceRepaint = aDC || TransparencyMode::Transparent == mTransparencyMode;
-  LayoutDeviceIntRegion region = GetRegionToPaint(forceRepaint, ps, hDC);
+  const bool forceRepaint = mTransparencyMode == TransparencyMode::Transparent;
+  const LayoutDeviceIntRegion region = GetRegionToPaint(forceRepaint, ps, hDC);
 
   RefPtr<nsWindow> strongThis(this);
 
@@ -250,7 +254,7 @@ bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
 
   bool didPaint = false;
   auto endPaint = MakeScopeExit([&] {
-  if (!aDC) {
+    if (!usingMemoryDC) {
       ::EndPaint(mWnd, &ps);
     }
     if (didPaint) {
@@ -259,7 +263,7 @@ bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
         listener->DidPaintWindow();
       }
       if (aNestingLevel == 0 && ::GetUpdateRect(mWnd, nullptr, false)) {
-    OnPaint(aDC, 1);
+        OnPaint(1);
       }
     }
   });
@@ -286,7 +290,7 @@ bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
       RefPtr<gfxASurface> targetSurface;
 
       // don't support transparency for non-GDI rendering, for now
-      if (IsPopup() && TransparencyMode::Transparent == mTransparencyMode) {
+      if (TransparencyMode::Transparent == mTransparencyMode) {
         // This mutex needs to be held when EnsureTransparentSurface is
         // called.
         MutexAutoLock lock(mBasicLayersSurface->GetTransparentSurfaceLock());
@@ -316,11 +320,11 @@ bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
       // don't need to double buffer with anything but GDI
       BufferMode doubleBuffering = mozilla::layers::BufferMode::BUFFER_NONE;
       switch (mTransparencyMode) {
-          case TransparencyMode::BorderlessGlass:
-          default:
-            // If we're not doing translucency, then double buffer
-            doubleBuffering = mozilla::layers::BufferMode::BUFFERED;
-            break;
+        case TransparencyMode::BorderlessGlass:
+        default:
+          // If we're not doing translucency, then double buffer
+          doubleBuffering = mozilla::layers::BufferMode::BUFFERED;
+          break;
         case TransparencyMode::Transparent:
           // If we're rendering with translucency, we're going to be
           // rendering the whole window; make sure we clear it first
@@ -338,7 +342,7 @@ bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
         }
       }
 
-      if (IsPopup() && TransparencyMode::Transparent == mTransparencyMode) {
+      if (TransparencyMode::Transparent == mTransparencyMode) {
         // Data from offscreen drawing surface was copied to memory bitmap of
         // transparent bitmap. Now it can be read from memory bitmap to apply
         // alpha channel and after that displayed on the screen.
@@ -349,8 +353,8 @@ bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
       if (nsIWidgetListener* listener = GetPaintListener()) {
         result = listener->PaintWindow(this, region);
       }
-        if (!gfxEnv::MOZ_DISABLE_FORCE_PRESENT() &&
-            gfxWindowsPlatform::GetPlatform()->DwmCompositionEnabled()) {
+      if (!gfxEnv::MOZ_DISABLE_FORCE_PRESENT() &&
+          gfxWindowsPlatform::GetPlatform()->DwmCompositionEnabled()) {
         nsCOMPtr<nsIRunnable> event = NewRunnableMethod(
             "nsWindow::ForcePresent", this, &nsWindow::ForcePresent);
         NS_DispatchToMainThread(event);

9d82a3f5b7bdbc6b76fece91ab26fc2b66328e7f
K4sum1 <k4sum1@eclipse.cx>
2025-05-20 19:02:38 -0500

    idk why I need to do this

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -350,7 +350,7 @@ static SystemTimeConverter<DWORD>& TimeConverter() {
 // Global event hook for window cloaking. Never deregistered.
 //  - `Nothing` if not yet set.
 //  - `Some(nullptr)` if no attempt should be made to set it.
-static mozilla::Maybe<HWINEVENTHOOK> sWinCloakEventHook =
+MOZ_RUNINIT static mozilla::Maybe<HWINEVENTHOOK> sWinCloakEventHook =
     IsWin8OrLater() ? Nothing() : Some(HWINEVENTHOOK(nullptr));
 static mozilla::LazyLogModule sCloakingLog("DWMCloaking");
 
@@ -601,7 +601,7 @@ class TIPMessageHandler {
     return static_cast<LRESULT>(TRUE);
   }
 
-  static WindowsDllInterceptor sTipTsfInterceptor;
+  MOZ_RUNINIT static WindowsDllInterceptor sTipTsfInterceptor;
   static WindowsDllInterceptor::FuncHookType<WINEVENTPROC>
       sProcessCaretEventsStub;
   static WindowsDllInterceptor::FuncHookType<decltype(&SendMessageTimeoutW)>

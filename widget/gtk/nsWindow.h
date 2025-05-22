5047148c3564510baebe7227ec29c7eac4fc5bb2
K4sum1 <k4sum1@eclipse.cx>
2025-05-22 17:17:49 -0500

    Bug 1933958 - Add a test for the widget opaque region. r=gfx-reviewers,aosmond    2024-11-28 23:09:12    6f6de92999bfdf4d34e6c8a5a98d1ca1eb346a9e

--- widget/gtk/nsWindow.h
+++ widget/gtk/nsWindow.h
@@ -294,9 +294,6 @@ class nsWindow final : public nsBaseWidget {
 
   void UpdateOpaqueRegionInternal();
   void UpdateOpaqueRegion(const LayoutDeviceIntRegion&) override;
-  LayoutDeviceIntRegion GetOpaqueRegionForTesting() const override {
-    return GetOpaqueRegion();
-  }
   LayoutDeviceIntRegion GetOpaqueRegion() const;
 
   already_AddRefed<mozilla::gfx::DrawTarget> StartRemoteDrawingInRegion(

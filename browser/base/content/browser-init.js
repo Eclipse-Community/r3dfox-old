15827721a8c37b182a857712d252d78ad4904a1f
K4sum1 <k4sum1@eclipse.cx>
2025-05-22 17:25:14 -0500

    Bug 1846845 - Remove some unneeded isPlatformAndVersionAtLeast checks r=Gijs    2023-08-03 06:30:37    31e3d8a68ad8bf96af5dd9998c61476651abee1d

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -442,7 +442,9 @@ var gBrowserInit = {
     }
 
     FullScreen.init();
+    if (AppConstants.isPlatformAndVersionAtLeast("win", "10")) {
     MenuTouchModeObserver.init();
+    }
 
     if (AppConstants.MOZ_DATA_REPORTING) {
       gDataNotificationInfoBar.init();
@@ -1078,7 +1080,9 @@ var gBrowserInit = {
       );
       Services.obs.removeObserver(gKeywordURIFixup, "keyword-uri-fixup");
 
+    if (AppConstants.isPlatformAndVersionAtLeast("win", "10")) {
       MenuTouchModeObserver.uninit();
+    }
       BrowserOffline.uninit();
       CanvasPermissionPromptHelper.uninit();
       WebAuthnPromptHelper.uninit();

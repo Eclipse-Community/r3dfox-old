c2239355c8ba7d4774deb6008556f44a48c8084d
K4sum1 <k4sum1@eclipse.cx>
2025-05-27 10:56:39 -0500

    Add back Ogg Theora Video support

--- dom/media/platforms/PDMFactory.cpp
+++ dom/media/platforms/PDMFactory.cpp
@@ -19,6 +19,7 @@
 #include "MP4Decoder.h"
 #include "MediaChangeMonitor.h"
 #include "MediaInfo.h"
+#include "TheoraDecoder.h"
 #include "VPXDecoder.h"
 #include "VideoUtils.h"
 #include "mozilla/ClearOnShutdown.h"
@@ -869,6 +870,9 @@ DecodeSupportSet PDMFactory::SupportsMimeType(
       return MCSInfo::GetDecodeSupportSet(MediaCodec::AV1, aSupported);
     }
 #endif
+    if (TheoraDecoder::IsTheora(aMimeType)) {
+      return MCSInfo::GetDecodeSupportSet(MediaCodec::Theora, aSupported);
+    }
     if (MP4Decoder::IsHEVC(aMimeType)) {
       return MCSInfo::GetDecodeSupportSet(MediaCodec::HEVC, aSupported);
     }
@@ -901,6 +905,7 @@ DecodeSupportSet PDMFactory::SupportsMimeType(
 bool PDMFactory::AllDecodersAreRemote() {
   return StaticPrefs::media_rdd_process_enabled() &&
          StaticPrefs::media_rdd_opus_enabled() &&
+         StaticPrefs::media_rdd_theora_enabled() &&
          StaticPrefs::media_rdd_vorbis_enabled() &&
          StaticPrefs::media_rdd_vpx_enabled() &&
 #if defined(MOZ_WMF)

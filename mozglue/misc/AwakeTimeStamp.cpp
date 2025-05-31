1a564744e0bdb9625f4c2dd842ac866c0b6753e4
K4sum1 <k4sum1@eclipse.cx>
2025-05-31 08:49:59 -0500

    Bug 1205985 - Implement something to get the process uptime with and without the time the device was suspended. r=haik,dmajor,jld    2021/1/12 20:34:59    b8fa2682851f934b2ea2652d0396b605d61776df

--- mozglue/misc/AwakeTimeStamp.cpp
+++ mozglue/misc/AwakeTimeStamp.cpp
@@ -67,11 +67,15 @@ AwakeTimeStamp AwakeTimeStamp::NowLoRes() {
 static constexpr uint64_t kHNSperUS = 10;
 
 AwakeTimeStamp AwakeTimeStamp::NowLoRes() {
-  ULONGLONG interrupt_time;
-  DebugOnly<bool> rv = QueryUnbiasedInterruptTime(&interrupt_time);
-  MOZ_ASSERT(rv);
+LARGE_INTEGER ticks;
+LARGE_INTEGER Frequency;
 
-  return AwakeTimeStamp(interrupt_time / kHNSperUS);
+QueryPerformanceFrequency(&Frequency); 
+QueryPerformanceCounter(&ticks);
+ticks.QuadPart *= 1000000;
+ticks.QuadPart /= Frequency.QuadPart;
+
+  return AwakeTimeStamp(ticks.QuadPart);
 }
 
 #else  // Linux and other POSIX but not macOS

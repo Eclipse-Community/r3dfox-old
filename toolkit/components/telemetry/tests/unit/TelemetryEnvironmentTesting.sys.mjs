66fe13f87518be698a730ea0c25f25ce7507d87d
K4sum1 <k4sum1@eclipse.cx>
2025-05-31 08:54:48 -0500

    Bug 1943698 - Remove Legacy Environment fields that never report a value r=TravisLong,florian    2025-02-04 04:37:15    03f935245d8e9851a61b3f0e28a3285b320182ba

--- toolkit/components/telemetry/tests/unit/TelemetryEnvironmentTesting.sys.mjs
+++ toolkit/components/telemetry/tests/unit/TelemetryEnvironmentTesting.sys.mjs
@@ -270,6 +270,7 @@ export var TelemetryEnvironmentTesting = {
       fissionEnabled: "boolean",
       intl: "object",
       locale: "string",
+      telemetryEnabled: "boolean",
       update: "object",
       userPrefs: "object",
     };
@@ -682,6 +683,8 @@ export var TelemetryEnvironmentTesting = {
           "windowsUBR must be null or a number."
         );
       }
+    } else if (gIsAndroid) {
+      lazy.Assert.ok(this.checkNullOrString(osData.kernelVersion));
     } else if (gIsLinux) {
       lazy.Assert.ok(this.checkNullOrString(osData.distro));
       lazy.Assert.ok(this.checkNullOrString(osData.distroVersion));
@@ -697,12 +700,19 @@ export var TelemetryEnvironmentTesting = {
     lazy.Assert.ok("D2DEnabled" in gfxData);
     lazy.Assert.ok("DWriteEnabled" in gfxData);
     lazy.Assert.ok("Headless" in gfxData);
+    lazy.Assert.ok("EmbeddedInFirefoxReality" in gfxData);
     lazy.Assert.ok("TargetFrameRate" in gfxData);
     lazy.Assert.equal(typeof gfxData.TargetFrameRate, "number");
     lazy.Assert.ok("textScaleFactor" in gfxData);
+    // DWriteVersion is disabled due to main thread jank and will be enabled
+    // again as part of bug 1154500.
+    // Assert.ok("DWriteVersion" in gfxData);
     if (gIsWindows) {
       lazy.Assert.equal(typeof gfxData.D2DEnabled, "boolean");
       lazy.Assert.equal(typeof gfxData.DWriteEnabled, "boolean");
+      lazy.Assert.equal(typeof gfxData.EmbeddedInFirefoxReality, "boolean");
+      // As above, will be enabled again as part of bug 1154500.
+      // Assert.ok(this.checkString(gfxData.DWriteVersion));
     }
 
     lazy.Assert.ok("adapters" in gfxData);

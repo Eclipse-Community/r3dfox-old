ab56a07128aef45bde3830c4e11c107e8b6ee8c2
K4sum1 <k4sum1@eclipse.cx>
2025-05-31 08:40:54 -0500

    Bug 1946269 - Fix async screenshots accessor in layer compositor mode r=gfx-reviewers,lsalzman    2025-02-07 03:40:27    f8b39c1089385fd04833362ce0eea6a369ceb060

--- gfx/webrender_bindings/RenderCompositorANGLE.cpp
+++ gfx/webrender_bindings/RenderCompositorANGLE.cpp
@@ -817,11 +817,6 @@ bool RenderCompositorANGLE::UseCompositor() const {
 }
 
 bool RenderCompositorANGLE::SupportAsyncScreenshot() {
-#ifdef NIGHTLY_BUILD
-  if (StaticPrefs::gfx_webrender_layer_compositor_AtStartup()) {
-    return true;
-  }
-#endif
   return !UseCompositor() && !mDisablingNativeCompositor;
 }
 

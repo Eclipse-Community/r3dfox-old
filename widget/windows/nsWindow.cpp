aa9d95edf0283f08b70766b5f42a850d0c8a61cb
K4sum1 <k4sum1@eclipse.cx>
2025-05-31 08:41:28 -0500

    Bug 1934375 - Take over whole non-client area for PiP windows again, for now. r=win-reviewers,gstoll    2025-01-31 02:36:09    5f02e0783538c760dcd3dd675303c9655bb012fd

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -666,7 +666,6 @@ nsWindow::nsWindow(bool aIsChildWindow)
     : nsBaseWidget(BorderStyle::Default),
       mFrameState(std::in_place, this),
       mIsChildWindow(aIsChildWindow),
-      mPIPWindow(false),
       mMicaBackdrop(false),
       mLastPaintEndTime(TimeStamp::Now()),
       mCachedHitTestTime(TimeStamp::Now()),
@@ -887,7 +886,6 @@ nsresult nsWindow::Create(nsIWidget* aParent, const LayoutDeviceIntRect& aRect,
       aParent ? (HWND)aParent->GetNativeData(NS_NATIVE_WINDOW) : nullptr;
 
   mIsRTL = aInitData->mRTL;
-  mPIPWindow = aInitData->mPIPWindow;
   mOpeningAnimationSuppressed = aInitData->mIsAnimationSuppressed;
   mAlwaysOnTop = aInitData->mAlwaysOnTop;
   mIsAlert = aInitData->mIsAlert;
@@ -2703,9 +2701,6 @@ bool nsWindow::UpdateNonClientMargins(bool aReflowWindow) {
         }
       }
     }
-  } else if (mPIPWindow &&
-             !StaticPrefs::widget_windows_pip_decorations_enabled()) {
-    metrics.mOffset = metrics.DefaultMargins();
   } else {
     metrics.mOffset = NormalWindowNonClientOffset();
   }

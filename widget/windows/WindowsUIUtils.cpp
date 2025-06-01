bb6743ac36fd9c6059706bc6aab367910d4aa55f
K4sum1 <k4sum1@eclipse.cx>
2025-05-31 21:58:49 -0500

    Bug 1844908 - Remove pre-Win10-specific codepath from browser/app/winlauncher/. r=mhowell    2023-07-26 16:52:50    8d754481cce705f5eee9eab63310c14b2cb7d0fb Bug 1844908 - Remove pre-Win10-specific codepath from browser/components/shell/. r=nalexander    2023-07-26 16:52:50    016a8659db2448b53b0089c0a4811e4f88462424 Bug 1844908 - Remove pre-Win10-specific codepath from dom/media/. r=alwu    2023-07-26 16:52:50    ba0ba2b455ed1712fd9213faa1f93787baea4ccc Bug 1844908 - Remove pre-Win10-specific codepath from hal/. r=gsvelto    2023-07-26 16:52:51    a5e6f6c1ab4aa061660e8d471af7624f3d439d85 Bug 1844908 - Remove pre-Win10-specific codepath from ipc/. r=ipc-reviewers,nika    2023-07-26 16:52:51    1518105f40f725976df402950e31763bc5053972 Bug 1844908 - Remove pre-Win10-specific codepath from memory/. r=glandium    2023-07-26 16:52:51    4a8202423936f7ed5a5ba8af7cca745c57cd8262 Bug 1844908 - Remove pre-Win10-specific codepath from mozglue/. r=glandium    2023-07-26 16:52:52    ccc00ef839a24cd529dc015c80a09c7a393aea18 Bug 1844908 - Remove pre-Win10-specific codepath from security/manager/. r=keeler    2023-07-26 16:52:52    5b8d92872e153dfb0e3a3aee3a81416a33b6d9bd Bug 1844908 - Remove pre-Win10-specific codepath from security/sandbox/win/. r=bobowen    2023-07-26 16:52:52    58163def5108b4d9d121ce721a7b67734b4032c5 Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/components/parentalcontrols/. r=handyman    2023-07-26 16:52:52    8a6d9a46d0c9b2f35adc814995e05e69ffc5d91b Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/crashreporter/. r=gsvelto    2023-07-26 16:52:52    a79c93429cdf4f346ecdb2d10bf8cc887867c897 Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/mozapps/defaultagent/. r=nrishel    2023-07-26 16:52:53    44c2deaf1a155eb8bc74349b9ed030c0f2ca68d9 Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/system/windowsPackageManager/. r=bhearsum,nrishel    2023-07-26 16:52:53    f0584032a39e1bf2fa97995f91a40bce0ec971d0 Bug 1844908 - Remove pre-Win10-specific codepath from DLL Blocklist Services. r=gstoll    2023-07-26 16:52:53    aba7dab929c2d44c76f48aa7ea97ad143dbdaeff Bug 1844908 - Remove pre-Win10-specific codepath from Gecko Profiler. r=mstange    2023-07-26 16:52:53    83b286649bd3e984857ffdf73fba1d2a8a59ad00 Bug 1844908 - Remove pre-Win10-specific codepath from uriloader/exthandler/. r=Gijs    2023-07-26 16:52:53    f37aa4efb5c29465a54406d550e5d6ea177012d7 Bug 1844908 - Remove pre-Win10-specific codepath from widget/. r=handyman    2023-07-26 16:52:54    9ec38aeba11979e584f702ca603f81e3c4c6625b Bug 1844908 - Remove pre-Win10-specific codepath from xpcom/. r=emilio    2023-07-26 16:52:54    c5739e2655474a52ae4ce42d42dbda32a46a7178

--- widget/windows/WindowsUIUtils.cpp
+++ widget/windows/WindowsUIUtils.cpp
@@ -333,6 +333,11 @@ static IInspectable* GetUISettings() {
   // expected, sigh.
   static StaticRefPtr<IInspectable> sUiSettingsAsInspectable;
 
+  if (!IsWin10OrLater()) {
+    // Windows.UI.ViewManagement.UISettings is Win10+ only.
+    return nullptr;
+  }
+
   if (!sUiSettingsAsInspectable) {
     ComPtr<IInspectable> uiSettingsAsInspectable;
     ::RoActivateInstance(
@@ -586,6 +591,10 @@ void WindowsUIUtils::UpdateInWin10TabletMode() {
   MOZ_DIAGNOSTIC_ASSERT(NS_IsMainThread());
 
 #ifndef __MINGW32__
+  if (!IsWin10OrLater()) {
+    return;
+  }
+
   nsresult rv;
   nsCOMPtr<nsIWindowMediator> winMediator(
       do_GetService(NS_WINDOWMEDIATOR_CONTRACTID, &rv));
@@ -893,6 +902,10 @@ Result<HStringUniquePtr, HRESULT> ConvertToWindowsString(
 
 static Result<Ok, nsresult> RequestShare(
     const std::function<HRESULT(IDataRequestedEventArgs* pArgs)>& aCallback) {
+  if (!IsWin10OrLater()) {
+    return Err(NS_ERROR_FAILURE);
+  }
+
   HWND hwnd = GetForegroundWindow();
   if (!hwnd) {
     return Err(NS_ERROR_FAILURE);

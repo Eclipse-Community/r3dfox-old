c7f6ecc24ae380c2bc01abb1046e0ea7b9b9e32a
K4sum1 <k4sum1@eclipse.cx>
2025-05-31 20:06:18 -0500

    Bug 1866022 - Teach GTK to render non-native tree columns and toolbars. r=dao,stransky,desktop-theme-reviewers    2023-12-05 02:57:35    4d0e7147951eaafe847f82f9db94e766ff7845c2

--- widget/windows/nsLookAndFeel.cpp
+++ widget/windows/nsLookAndFeel.cpp
@@ -268,9 +268,6 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme,
     case ColorID::MozButtonhoverface:
     case ColorID::MozButtonactiveface:
     case ColorID::MozButtondisabledface:
-    case ColorID::MozColheader:
-    case ColorID::MozColheaderhover:
-    case ColorID::MozColheaderactive:
       idx = COLOR_BTNFACE;
       break;
     case ColorID::Buttonhighlight:
@@ -388,7 +385,6 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme,
     case ColorID::MozDialogtext:
     case ColorID::MozColheadertext:
     case ColorID::MozColheaderhovertext:
-    case ColorID::MozColheaderactivetext:
       idx = COLOR_WINDOWTEXT;
       break;
     case ColorID::Visitedtext: {

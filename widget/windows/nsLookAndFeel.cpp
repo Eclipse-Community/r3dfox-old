e0d302e458e5a90aa91e734326af65dac7c76749
K4sum1 <k4sum1@eclipse.cx>
2025-05-31 21:59:20 -0500

    Bug 1843663 - Remove pre windows-10-specific media queries and styles. r=dao,Gijs,handyman,settings-reviewers    2023-07-18 22:11:33    7e60f84b6ec7c303e07f303a4838e907265a9a05

--- widget/windows/nsLookAndFeel.cpp
+++ widget/windows/nsLookAndFeel.cpp
@@ -50,6 +50,30 @@ static int32_t GetTooltipOffsetVertical() {
                     float(cursorSize) / float(kDefaultCursorSize));
 }
 
+const int NUM_COMMAND_BUTTONS = 3;
+SIZE nsLookAndFeel::sCommandButtonMetrics[NUM_COMMAND_BUTTONS];
+bool nsLookAndFeel::sCommandButtonMetricsInitialized = false;
+SIZE nsLookAndFeel::sCommandButtonBoxMetrics;
+bool nsLookAndFeel::sCommandButtonBoxMetricsInitialized = false;
+
+bool nsLookAndFeel::sTitlebarInfoPopulatedAero = false;
+bool nsLookAndFeel::sTitlebarInfoPopulatedThemed = false;
+
+/**
+ * Windows themes we currently detect.
+ */
+enum class WindowsTheme {
+  Generic = 0,  // unrecognized theme
+  Classic,
+  Aero,
+  Luna,
+  Royale,
+  Zune,
+  AeroLite
+};
+
+static WindowsTheme sThemeId = WindowsTheme::Generic;
+
 UXThemeHandle::~UXThemeHandle() { Close(); }
 
 void UXThemeHandle::OpenOnce(LPCWSTR aClassList) {
@@ -117,6 +141,165 @@ static const wchar_t* GetUXThemeClassName(UXThemeClass aClass) {
   return L"";
 }
 
+// static
+void nsLookAndFeel::EnsureCommandButtonMetrics() {
+  if (sCommandButtonMetricsInitialized) {
+    return;
+  }
+  sCommandButtonMetricsInitialized = true;
+
+  // This code should never need to be evaluated for our UI since if we need
+  // these metrics for our UI we should make sure that we obtain the correct
+  // metrics when nsWindow::Create() is called.  The generic metrics that we
+  // fetch here will likley not match the current theme, but we provide these
+  // values in case arbitrary content is styled with the '-moz-appearance'
+  // value '-moz-window-button-close' etc.
+  //
+  // ISSUE: We'd prefer to use MOZ_ASSERT_UNREACHABLE here, but since content
+  // (and at least one of our crashtests) can use '-moz-window-button-close'
+  // we need to use NS_WARNING instead.
+  NS_WARNING("Making expensive and likely unnecessary GetSystemMetrics calls");
+
+  sCommandButtonMetrics[0].cx = GetSystemMetrics(SM_CXSIZE);
+  sCommandButtonMetrics[0].cy = GetSystemMetrics(SM_CYSIZE);
+  sCommandButtonMetrics[1].cx = sCommandButtonMetrics[2].cx =
+      sCommandButtonMetrics[0].cx;
+  sCommandButtonMetrics[1].cy = sCommandButtonMetrics[2].cy =
+      sCommandButtonMetrics[0].cy;
+
+  // Trigger a refresh on the next layout.
+  sTitlebarInfoPopulatedAero = sTitlebarInfoPopulatedThemed = false;
+}
+
+// static
+void nsLookAndFeel::EnsureCommandButtonBoxMetrics() {
+  if (sCommandButtonBoxMetricsInitialized) {
+    return;
+  }
+  sCommandButtonBoxMetricsInitialized = true;
+
+  EnsureCommandButtonMetrics();
+
+  sCommandButtonBoxMetrics.cx = sCommandButtonMetrics[0].cx +
+                                sCommandButtonMetrics[1].cx +
+                                sCommandButtonMetrics[2].cx;
+  sCommandButtonBoxMetrics.cy = sCommandButtonMetrics[0].cy +
+                                sCommandButtonMetrics[1].cy +
+                                sCommandButtonMetrics[2].cy;
+
+  // Trigger a refresh on the next layout.
+  sTitlebarInfoPopulatedAero = sTitlebarInfoPopulatedThemed = false;
+}
+
+// static
+void nsLookAndFeel::UpdateTitlebarInfo(HWND aWnd) {
+  if (!aWnd) return;
+
+  if (!sTitlebarInfoPopulatedAero &&
+      gfxWindowsPlatform::GetPlatform()->DwmCompositionEnabled()) {
+    RECT captionButtons;
+    if (SUCCEEDED(DwmGetWindowAttribute(aWnd, DWMWA_CAPTION_BUTTON_BOUNDS,
+                                        &captionButtons,
+                                        sizeof(captionButtons)))) {
+      sCommandButtonBoxMetrics.cx =
+          captionButtons.right - captionButtons.left - 3;
+      sCommandButtonBoxMetrics.cy =
+          (captionButtons.bottom - captionButtons.top) - 1;
+      sCommandButtonBoxMetricsInitialized = true;
+      MOZ_ASSERT(
+          sCommandButtonBoxMetrics.cx > 0 && sCommandButtonBoxMetrics.cy > 0,
+          "We must not cache bad command button box dimensions");
+      sTitlebarInfoPopulatedAero = true;
+    }
+  }
+
+  // NB: sTitlebarInfoPopulatedThemed is always true pre-vista.
+  if (sTitlebarInfoPopulatedThemed || IsWin8OrLater()) return;
+
+  // Query a temporary, visible window with command buttons to get
+  // the right metrics.
+  WNDCLASSW wc;
+  wc.style = 0;
+  wc.lpfnWndProc = ::DefWindowProcW;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hInstance = nsToolkit::mDllInstance;
+  wc.hIcon = nullptr;
+  wc.hCursor = nullptr;
+  wc.hbrBackground = nullptr;
+  wc.lpszMenuName = nullptr;
+  wc.lpszClassName = kClassNameTemp;
+  ::RegisterClassW(&wc);
+
+  // Create a transparent descendant of the window passed in. This
+  // keeps the window from showing up on the desktop or the taskbar.
+  // Note the parent (browser) window is usually still hidden, we
+  // don't want to display it, so we can't query it directly.
+  HWND hWnd = CreateWindowExW(WS_EX_LAYERED, kClassNameTemp, L"",
+                              WS_OVERLAPPEDWINDOW, 0, 0, 0, 0, aWnd, nullptr,
+                              nsToolkit::mDllInstance, nullptr);
+  NS_ASSERTION(hWnd, "UpdateTitlebarInfo window creation failed.");
+
+  int showType = SW_SHOWNA;
+  // We try to avoid activating this window, but on Aero basic (aero without
+  // compositor) and aero lite (special theme for win server 2012/2013) we may
+  // get the wrong information if the window isn't activated, so we have to:
+  if (sThemeId == WindowsTheme::AeroLite ||
+      (sThemeId == WindowsTheme::Aero &&
+       !gfxWindowsPlatform::GetPlatform()->DwmCompositionEnabled())) {
+    showType = SW_SHOW;
+  }
+  ShowWindow(hWnd, showType);
+  TITLEBARINFOEX info = {0};
+  info.cbSize = sizeof(TITLEBARINFOEX);
+  SendMessage(hWnd, WM_GETTITLEBARINFOEX, 0, (LPARAM)&info);
+  DestroyWindow(hWnd);
+
+  // Only set if we have valid data for all three buttons we use.
+  if ((info.rgrect[2].right - info.rgrect[2].left) == 0 ||
+      (info.rgrect[3].right - info.rgrect[3].left) == 0 ||
+      (info.rgrect[5].right - info.rgrect[5].left) == 0) {
+    NS_WARNING("WM_GETTITLEBARINFOEX query failed to find usable metrics.");
+    return;
+  }
+  // minimize
+  sCommandButtonMetrics[0].cx = info.rgrect[2].right - info.rgrect[2].left;
+  sCommandButtonMetrics[0].cy = info.rgrect[2].bottom - info.rgrect[2].top;
+  // maximize/restore
+  sCommandButtonMetrics[1].cx = info.rgrect[3].right - info.rgrect[3].left;
+  sCommandButtonMetrics[1].cy = info.rgrect[3].bottom - info.rgrect[3].top;
+  // close
+  sCommandButtonMetrics[2].cx = info.rgrect[5].right - info.rgrect[5].left;
+  sCommandButtonMetrics[2].cy = info.rgrect[5].bottom - info.rgrect[5].top;
+  sCommandButtonMetricsInitialized = true;
+
+#ifdef DEBUG
+  // Verify that all values for the command buttons are positive values
+  // otherwise we have cached bad values for the caption buttons
+  for (int i = 0; i < NUM_COMMAND_BUTTONS; i++) {
+    MOZ_ASSERT(sCommandButtonMetrics[i].cx > 0);
+    MOZ_ASSERT(sCommandButtonMetrics[i].cy > 0);
+  }
+#endif
+
+  sTitlebarInfoPopulatedThemed = true;
+}
+
+// visual style (aero glass, aero basic)
+//    theme (aero, luna, zune)
+//      theme color (silver, olive, blue)
+//        system colors
+
+const struct {
+  LPCWSTR name;
+  WindowsTheme type;
+} kKnownThemes[] = {{L"aero.msstyles", WindowsTheme::Aero},
+                    {L"aerolite.msstyles", WindowsTheme::AeroLite},
+                    {L"luna.msstyles", WindowsTheme::Luna},
+                    {L"zune.msstyles", WindowsTheme::Zune},
+                    {L"royale.msstyles", WindowsTheme::Royale}};
+
+
 bool nsLookAndFeel::sIsDefaultWindowsTheme = false;
 HANDLE nsLookAndFeel::GetTheme(UXThemeClass aClass) {
   auto& handle =
@@ -157,6 +340,9 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme,
   EnsureInit();
 
   auto UseNonNativeMenuColors = [&]() -> bool {
+  if (!LookAndFeel::WindowsNonNativeMenusEnabled()) {
+    return false;
+  }
   return LookAndFeel::GetInt(LookAndFeel::IntID::WindowsDefaultTheme) || aScheme == ColorScheme::Dark;
   };
 
@@ -188,6 +374,9 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme,
       case ColorID::MozMenubarhovertext:
         if (UseNonNativeMenuColors()) {
           return false;
+        }
+        if (!nsLookAndFeel::IsAppThemed()) {
+          return nsLookAndFeel::AreFlatMenusEnabled();
         }
         [[fallthrough]];
       case ColorID::MozMenuhovertext:
@@ -292,6 +481,10 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme,
       if (UseNonNativeMenuColors()) {
         aColor = kNonNativeMenuText;
         return NS_OK;
+      }
+      if (!nsLookAndFeel::IsAppThemed()) {
+        idx = COLOR_MENUTEXT;
+        break;
       }
       [[fallthrough]];
     case ColorID::MozMenuhovertext:
@@ -574,11 +767,22 @@ nsresult nsLookAndFeel::NativeGetInt(IntID aID, int32_t& aResult) {
     case IntID::TreeScrollLinesMax:
       aResult = 3;
       break;
+    case IntID::WindowsClassic:
+      aResult = !nsLookAndFeel::IsAppThemed();
+      break;
     case IntID::WindowsDefaultTheme:
       aResult = sIsDefaultWindowsTheme;
       break;
+    case IntID::DWMCompositor:
+      aResult = gfxWindowsPlatform::GetPlatform()->DwmCompositionEnabled();
+      break;
     case IntID::WindowsAccentColorInTitlebar:
       aResult = mTitlebarColors.mUseAccent;
+      break;
+    case IntID::WindowsGlass:
+      // Aero Glass is only available prior to Windows 8 when DWM is used.
+      aResult = (gfxWindowsPlatform::GetPlatform()->DwmCompositionEnabled() &&
+                 !IsWin8OrLater());
       break;
     case IntID::WindowsMica:
       aResult = WinUtils::MicaEnabled();
@@ -1043,12 +1247,14 @@ void nsLookAndFeel::EnsureInit() {
 
   const bool neededMicaWorkaround = NeedsMicaWorkaround();
 
-  mColorMenuHoverText = ::GetColorFromTheme(UXThemeClass::Menu, MENU_POPUPITEM,
-                                            MPI_HOT, TMT_TEXTCOLOR);
-  mColorMediaText =
-      ::GetColorFromTheme(UXThemeClass::MediaToolbar, TP_BUTTON, TS_NORMAL, TMT_TEXTCOLOR);
-  mColorCommunicationsText = ::GetColorFromTheme(
-      UXThemeClass::CommunicationsToolbar, TP_BUTTON, TS_NORMAL, TMT_TEXTCOLOR);
+  if (nsLookAndFeel::IsAppThemed()) {
+    mColorMenuHoverText = ::GetColorFromTheme(UXThemeClass::Menu, MENU_POPUPITEM,
+                                              MPI_HOT, TMT_TEXTCOLOR);
+    mColorMediaText =
+        ::GetColorFromTheme(UXThemeClass::MediaToolbar, TP_BUTTON, TS_NORMAL, TMT_TEXTCOLOR);
+    mColorCommunicationsText = ::GetColorFromTheme(
+         UXThemeClass::CommunicationsToolbar, TP_BUTTON, TS_NORMAL, TMT_TEXTCOLOR);
+  }
 
   // Fill out the sys color table.
   for (int i = SYS_COLOR_MIN; i <= SYS_COLOR_MAX; ++i) {
@@ -1104,5 +1310,71 @@ void nsLookAndFeel::EnsureInit() {
     WinUtils::UpdateMicaInAllWindows();
   }
 }
+
+// static
+void nsLookAndFeel::UpdateNativeThemeInfo() {
+  // Trigger a refresh of themed button metrics if needed
+  sTitlebarInfoPopulatedThemed = false;
+
+  bool mHighContrastOn = false;
+  sIsDefaultWindowsTheme = false;
+  sThemeId = WindowsTheme::Generic;
+
+  HIGHCONTRAST highContrastInfo;
+  highContrastInfo.cbSize = sizeof(HIGHCONTRAST);
+  if (SystemParametersInfo(SPI_GETHIGHCONTRAST, 0, &highContrastInfo, 0)) {
+    mHighContrastOn = ((highContrastInfo.dwFlags & HCF_HIGHCONTRASTON) != 0);
+  } else {
+    mHighContrastOn = false;
+  }
+
+  if (!nsLookAndFeel::IsAppThemed()) {
+    sThemeId = WindowsTheme::Classic;
+    return;
+  }
+
+  WCHAR themeFileName[MAX_PATH + 1];
+  WCHAR themeColor[MAX_PATH + 1];
+  if (FAILED(GetCurrentThemeName(themeFileName, MAX_PATH, themeColor, MAX_PATH,
+                                 nullptr, 0))) {
+    sThemeId = WindowsTheme::Classic;
+    return;
+  }
+
+  LPCWSTR themeName = wcsrchr(themeFileName, L'\\');
+  themeName = themeName ? themeName + 1 : themeFileName;
+
+  sThemeId = [&] {
+    for (const auto& theme : kKnownThemes) {
+      if (!lstrcmpiW(themeName, theme.name)) {
+        return theme.type;
+      }
+    }
+    return WindowsTheme::Generic;
+  }();
+
+  // We're using the default theme if we're using any of Aero, Aero Lite, or
+  // luna. However, on Win8, GetCurrentThemeName (see above) returns
+  // AeroLite.msstyles for the 4 builtin highcontrast themes as well. Those
+  // themes "don't count" as default themes, so we specifically check for high
+  // contrast mode in that situation.
+  sIsDefaultWindowsTheme = [&] {
+    if (mHighContrastOn && IsWin8OrLater()) {
+      return false;
+    }
+    return sThemeId == WindowsTheme::Aero ||
+           sThemeId == WindowsTheme::AeroLite || sThemeId == WindowsTheme::Luna;
+  }();
+}
+
+// static
+bool nsLookAndFeel::AreFlatMenusEnabled() {
+  BOOL useFlat = FALSE;
+  return !!::SystemParametersInfo(SPI_GETFLATMENU, 0, &useFlat, 0) ? useFlat
+                                                                   : false;
+}
+
+// static
+bool nsLookAndFeel::IsAppThemed() { return !!::IsAppThemed(); }
 
 #undef AVG2

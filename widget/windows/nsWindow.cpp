3db8dfdc276521390b08f267a788c9a5a893f6c9
K4sum1 <k4sum1@eclipse.cx>
2025-06-02 02:08:38 -0500

    Reapply "Add back nsUxThemeData to fix native controls"
    This reverts commit 364a84172747b5fad5f23435bedb89bdd7313f93.

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -737,7 +737,7 @@ nsWindow::nsWindow(bool aIsChildWindow)
     NS_ASSERTION(sIsOleInitialized, "***** OLE is not initialized!\n");
     MouseScrollHandler::Initialize();
     // Init theme data
-    nsLookAndFeel::UpdateNativeThemeInfo();
+    nsUXThemeData::UpdateNativeThemeInfo();
     RedirectedKeyDownMessageManager::Forget();
     if (mPointerEvents.ShouldEnableInkCollector()) {
       InkCollector::sInkCollector = new InkCollector();
@@ -795,8 +795,8 @@ static bool ShouldCacheTitleBarInfo(WindowType aWindowType,
   return (aWindowType == WindowType::TopLevel) &&
          (aBorderStyle == BorderStyle::Default ||
           aBorderStyle == BorderStyle::All) &&
-         (!nsLookAndFeel::sTitlebarInfoPopulatedThemed ||
-          !nsLookAndFeel::sTitlebarInfoPopulatedAero);
+         (!nsUXThemeData::sTitlebarInfoPopulatedThemed ||
+          !nsUXThemeData::sTitlebarInfoPopulatedAero);
 }
 
 void nsWindow::SendAnAPZEvent(InputData& aEvent) {
@@ -1182,7 +1182,7 @@ nsresult nsWindow::Create(nsIWidget* aParent, const LayoutDeviceIntRect& aRect,
   // Query for command button metric data for rendering the titlebar. We
   // only do this once on the first window that has an actual titlebar
   if (ShouldCacheTitleBarInfo(mWindowType, mBorderStyle)) {
-    nsLookAndFeel::UpdateTitlebarInfo(mWnd);
+    nsUXThemeData::UpdateTitlebarInfo(mWnd);
   }
 
   static bool a11yPrimed = false;
@@ -3221,7 +3221,7 @@ void nsWindow::UpdateOpaqueRegion(const LayoutDeviceIntRegion& aOpaqueRegion) {
       // The minimum glass height must be the caption buttons height,
       // otherwise the buttons are drawn incorrectly.
       largest.MoveToY(std::max<uint32_t>(
-          largest.Y(), nsLookAndFeel::GetCommandButtonBoxMetrics().cy));
+          largest.Y(), nsUXThemeData::GetCommandButtonBoxMetrics().cy));
     }
     margins.cyTopHeight = largest.Y();
   }
@@ -5246,7 +5246,7 @@ bool nsWindow::ProcessMessageInternal(UINT msg, WPARAM& wParam, LPARAM& lParam,
     case WM_THEMECHANGED: {
       // Update non-client margin offsets
       UpdateNonClientMargins();
-      nsLookAndFeel::UpdateNativeThemeInfo();
+      nsUXThemeData::UpdateNativeThemeInfo();
 
       // Invalidate the window so that the repaint will
       // pick up the new theme.

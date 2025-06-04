f499b5b6caa558bd409c315c47d712668d4d8d73
K4sum1 <k4sum1@eclipse.cx>
2025-06-04 04:05:46 -0500

    Bug 1337299 - Remove old version compatibility into WinUtils. r=jimm    2017-02-10 11:06:23    1b1e8ccc2f93160f2796fe0f6cd404a5bede79e5

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -667,7 +667,10 @@ class InitializeVirtualDesktopManagerTask : public Task {
 // Ground-truth query: does Windows claim the window is cloaked right now?
 static bool IsCloaked(HWND hwnd) {
   DWORD cloakedState;
-  HRESULT hr = ::DwmGetWindowAttribute(hwnd, DWMWA_CLOAKED, &cloakedState,
+  if (!WinUtils::dwmGetWindowAttributePtr) {
+    return false;
+  }
+  HRESULT hr = WinUtils::dwmGetWindowAttributePtr(hwnd, DWMWA_CLOAKED, &cloakedState,
                                        sizeof(cloakedState));
 
   if (FAILED(hr)) {
@@ -1119,16 +1122,16 @@ const wchar_t kShellLibraryName[] =  L"shell32.dll";
   // that we have a mWnd.
   mDefaultScale = -1.0;
 
-  if (mIsRTL) {
+  if (mIsRTL && WinUtils::dwmSetWindowAttributePtr) {
     DWORD dwAttribute = TRUE;
-    DwmSetWindowAttribute(mWnd, DWMWA_NONCLIENT_RTL_LAYOUT, &dwAttribute,
+    WinUtils::dwmSetWindowAttributePtr(mWnd, DWMWA_NONCLIENT_RTL_LAYOUT, &dwAttribute,
                           sizeof dwAttribute);
   }
 
   // Default to the system color scheme unless getting told otherwise.
   SetColorScheme(Nothing());
 
-  if (mOpeningAnimationSuppressed) {
+  if (mOpeningAnimationSuppressed && WinUtils::dwmSetWindowAttributePtr) {
     SuppressAnimation(true);
   }
 
@@ -1217,9 +1220,9 @@ const wchar_t kShellLibraryName[] =  L"shell32.dll";
 
 void nsWindow::LocalesChanged() {
   bool isRTL = intl::LocaleService::GetInstance()->IsAppLocaleRTL();
-  if (mIsRTL != isRTL) {
+  if (mIsRTL != isRTL && WinUtils::dwmSetWindowAttributePtr) {
     DWORD dwAttribute = isRTL;
-    DwmSetWindowAttribute(mWnd, DWMWA_NONCLIENT_RTL_LAYOUT, &dwAttribute,
+    WinUtils::dwmSetWindowAttributePtr(mWnd, DWMWA_NONCLIENT_RTL_LAYOUT, &dwAttribute,
                           sizeof dwAttribute);
     mIsRTL = isRTL;
   }
@@ -1768,7 +1771,7 @@ void nsWindow::Show(bool aState) {
     }
   }
 
-  if (mOpeningAnimationSuppressed) {
+  if (mOpeningAnimationSuppressed && WinUtils::dwmSetWindowAttributePtr) {
     SuppressAnimation(false);
   }
 }
@@ -2273,7 +2276,7 @@ void nsWindow::MoveToWorkspace(const nsAString& workspaceID) {
 
 void nsWindow::SuppressAnimation(bool aSuppress) {
   DWORD dwAttribute = aSuppress ? TRUE : FALSE;
-  DwmSetWindowAttribute(mWnd, DWMWA_TRANSITIONS_FORCEDISABLED, &dwAttribute,
+  WinUtils::dwmSetWindowAttributePtr(mWnd, DWMWA_TRANSITIONS_FORCEDISABLED, &dwAttribute,
                         sizeof dwAttribute);
 }
 
@@ -2634,9 +2637,9 @@ void nsWindow::SetColorScheme(const Maybe<ColorScheme>& aScheme) {
   }
   BOOL dark =
       aScheme.valueOrFrom(LookAndFeel::SystemColorScheme) == ColorScheme::Dark;
-  DwmSetWindowAttribute(mWnd, DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1, &dark,
+  WinUtils::dwmSetWindowAttributePtr(mWnd, DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1, &dark,
                         sizeof dark);
-  DwmSetWindowAttribute(mWnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &dark,
+  WinUtils::dwmSetWindowAttributePtr(mWnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &dark,
                         sizeof dark);
 }
 
@@ -2645,6 +2648,9 @@ void nsWindow::SetMicaBackdrop(bool aEnabled) {
     return;
   }
 
+  if (!WinUtils::dwmSetWindowAttributePtr) {
+    return;
+  }
   mMicaBackdrop = aEnabled;
   UpdateMicaBackdrop();
 }
@@ -2675,7 +2681,7 @@ void nsWindow::UpdateMicaBackdrop(bool aForce) {
         return DWMSBT_TABBEDWINDOW;
     }
   }();
-  ::DwmSetWindowAttribute(mWnd, DWMWA_SYSTEMBACKDROP_TYPE, &backdrop,
+  WinUtils::dwmSetWindowAttributePtr(mWnd, DWMWA_SYSTEMBACKDROP_TYPE, &backdrop,
                           sizeof backdrop);
   if (IsPopup()) {
     // For popups, we need a couple extra tweaks:
@@ -2686,7 +2692,7 @@ void nsWindow::UpdateMicaBackdrop(bool aForce) {
     //    acrylic). See also the WM_NCACTIVATE implementation.
     const DWM_WINDOW_CORNER_PREFERENCE corner =
         useBackdrop ? DWMWCP_ROUND : DWMWCP_DEFAULT;
-    ::DwmSetWindowAttribute(mWnd, DWMWA_WINDOW_CORNER_PREFERENCE, &corner,
+    WinUtils::dwmSetWindowAttributePtr(mWnd, DWMWA_WINDOW_CORNER_PREFERENCE, &corner,
                             sizeof corner);
     ::PostMessageW(mWnd, WM_NCACTIVATE, TRUE, -1);
   }
@@ -3293,8 +3299,8 @@ void nsWindow::UpdateGlass() {
 
   // Extends the window frame behind the client area
   if (gfxWindowsPlatform::GetPlatform()->DwmCompositionEnabled()) {
-    DwmExtendFrameIntoClientArea(mWnd, &margins);
-    DwmSetWindowAttribute(mWnd, DWMWA_NCRENDERING_POLICY, &policy,
+    WinUtils::dwmExtendFrameIntoClientAreaPtr(mWnd, &margins);
+    WinUtils::dwmSetWindowAttributePtr(mWnd, DWMWA_NCRENDERING_POLICY, &policy,
                           sizeof policy);
   }
 }
@@ -5151,7 +5157,7 @@ bool nsWindow::ProcessMessageInternal(UINT msg, WPARAM& wParam, LPARAM& lParam,
       /* We don't do this for win10 glass with a custom titlebar,
        * in order to avoid the caption buttons breaking. */
       !(IsWin10OrLater() && HasGlass()) &&
-      DwmDefWindowProc(mWnd, msg, wParam, lParam, &dwmHitResult)) {
+      WinUtils::dwmDwmDefWindowProcPtr(mWnd, msg, wParam, lParam, &dwmHitResult)) {
     *aRetValue = dwmHitResult;
     return true;
   }
@@ -6261,7 +6267,7 @@ bool nsWindow::ProcessMessageInternal(UINT msg, WPARAM& wParam, LPARAM& lParam,
       // updates when the value actually changes
       if (XRE_IsParentProcess()) {
         BOOL dwmEnabled = FALSE;
-        if (FAILED(::DwmIsCompositionEnabled(&dwmEnabled)) || !dwmEnabled) {
+        if (!WinUtils::dwmIsCompositionEnabledPtr || FAILED(WinUtils::dwmIsCompositionEnabledPtr(&dwmEnabled)) || !dwmEnabled) {
           gfxVars::SetDwmCompositionEnabled(false);
         } else {
           gfxVars::SetDwmCompositionEnabled(true);

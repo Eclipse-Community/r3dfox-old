4806711aea8aa2de503c8e2a5ea48eaf2d04399e
K4sum1 <k4sum1@eclipse.cx>
2025-06-04 03:51:57 -0500

    Bug 1959965 - Simplify nsWindow child set-up on Windows. r=win-reviewers,handyman    2025-04-15 03:00:16    802f14b748a591941bc8c2026983baeed0ebdfbd

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -662,9 +662,10 @@ static bool IsCloaked(HWND hwnd) {
  *
  **************************************************************/
 
-nsWindow::nsWindow()
+nsWindow::nsWindow(bool aIsChildWindow)
     : nsBaseWidget(BorderStyle::Default),
       mFrameState(std::in_place, this),
+      mIsChildWindow(aIsChildWindow),
       mPIPWindow(false),
       mMicaBackdrop(false),
       mLastPaintEndTime(TimeStamp::Now()),
@@ -1323,7 +1324,7 @@ DWORD nsWindow::WindowStyle() {
       break;
 
     case WindowType::Popup:
-      style = WS_OVERLAPPED | WS_POPUP | WS_CLIPCHILDREN;
+      style = WS_OVERLAPPED | WS_POPUP;
       break;
 
     default:
@@ -1337,6 +1338,13 @@ DWORD nsWindow::WindowStyle() {
   }
 
   style &= ~WindowStylesRemovedForBorderStyle(mBorderStyle);
+
+  if (mIsChildWindow) {
+    style |= WS_CLIPCHILDREN;
+    if (!(style & WS_POPUP)) {
+      style |= WS_CHILD;  // WS_POPUP and WS_CHILD are mutually exclusive.
+    }
+  }
 
   VERIFY_WINDOW_STYLE(style);
   return style;
@@ -6915,6 +6923,14 @@ bool nsWindow::ShouldUseOffMainThreadCompositing() {
   if (mWindowType == WindowType::Popup && mPopupType == PopupType::Tooltip) {
     return false;
   }
+
+  // Content rendering of popup is always done by child window.
+  // See nsDocumentViewer::ShouldAttachToTopLevel().
+  if (mWindowType == WindowType::Popup && !mIsChildWindow) {
+    MOZ_ASSERT(!mParent);
+    return false;
+  }
+
   return nsBaseWidget::ShouldUseOffMainThreadCompositing();
 }
 
@@ -8232,7 +8248,7 @@ already_AddRefed<nsIWidget> nsIWidget::CreateTopLevelWindow() {
 }
 
 already_AddRefed<nsIWidget> nsIWidget::CreateChildWindow() {
-  nsCOMPtr<nsIWidget> window = new nsWindow();
+  nsCOMPtr<nsIWidget> window = new nsWindow(true);
   return window.forget();
 }
 

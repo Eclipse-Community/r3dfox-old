d8aae7b514315ba7bcd8b6c5f075248b94238c6d
K4sum1 <k4sum1@eclipse.cx>
2025-06-04 04:01:53 -0500

    Bug 1844908 - Remove pre-Win10-specific codepath from browser/app/winlauncher/. r=mhowell    2023-07-26 16:52:50    8d754481cce705f5eee9eab63310c14b2cb7d0fb Bug 1844908 - Remove pre-Win10-specific codepath from browser/components/shell/. r=nalexander    2023-07-26 16:52:50    016a8659db2448b53b0089c0a4811e4f88462424 Bug 1844908 - Remove pre-Win10-specific codepath from dom/media/. r=alwu    2023-07-26 16:52:50    ba0ba2b455ed1712fd9213faa1f93787baea4ccc Bug 1844908 - Remove pre-Win10-specific codepath from hal/. r=gsvelto    2023-07-26 16:52:51    a5e6f6c1ab4aa061660e8d471af7624f3d439d85 Bug 1844908 - Remove pre-Win10-specific codepath from ipc/. r=ipc-reviewers,nika    2023-07-26 16:52:51    1518105f40f725976df402950e31763bc5053972 Bug 1844908 - Remove pre-Win10-specific codepath from memory/. r=glandium    2023-07-26 16:52:51    4a8202423936f7ed5a5ba8af7cca745c57cd8262 Bug 1844908 - Remove pre-Win10-specific codepath from mozglue/. r=glandium    2023-07-26 16:52:52    ccc00ef839a24cd529dc015c80a09c7a393aea18 Bug 1844908 - Remove pre-Win10-specific codepath from security/manager/. r=keeler    2023-07-26 16:52:52    5b8d92872e153dfb0e3a3aee3a81416a33b6d9bd Bug 1844908 - Remove pre-Win10-specific codepath from security/sandbox/win/. r=bobowen    2023-07-26 16:52:52    58163def5108b4d9d121ce721a7b67734b4032c5 Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/components/parentalcontrols/. r=handyman    2023-07-26 16:52:52    8a6d9a46d0c9b2f35adc814995e05e69ffc5d91b Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/crashreporter/. r=gsvelto    2023-07-26 16:52:52    a79c93429cdf4f346ecdb2d10bf8cc887867c897 Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/mozapps/defaultagent/. r=nrishel    2023-07-26 16:52:53    44c2deaf1a155eb8bc74349b9ed030c0f2ca68d9 Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/system/windowsPackageManager/. r=bhearsum,nrishel    2023-07-26 16:52:53    f0584032a39e1bf2fa97995f91a40bce0ec971d0 Bug 1844908 - Remove pre-Win10-specific codepath from DLL Blocklist Services. r=gstoll    2023-07-26 16:52:53    aba7dab929c2d44c76f48aa7ea97ad143dbdaeff Bug 1844908 - Remove pre-Win10-specific codepath from Gecko Profiler. r=mstange    2023-07-26 16:52:53    83b286649bd3e984857ffdf73fba1d2a8a59ad00 Bug 1844908 - Remove pre-Win10-specific codepath from uriloader/exthandler/. r=Gijs    2023-07-26 16:52:53    f37aa4efb5c29465a54406d550e5d6ea177012d7 Bug 1844908 - Remove pre-Win10-specific codepath from widget/. r=handyman    2023-07-26 16:52:54    9ec38aeba11979e584f702ca603f81e3c4c6625b Bug 1844908 - Remove pre-Win10-specific codepath from xpcom/. r=emilio    2023-07-26 16:52:54    c5739e2655474a52ae4ce42d42dbda32a46a7178

--- dom/media/platforms/PDMFactory.cpp
+++ dom/media/platforms/PDMFactory.cpp
@@ -38,6 +38,7 @@
 
 #ifdef XP_WIN
 #  include "WMFDecoderModule.h"
+#  include "mozilla/WindowsVersion.h"
 #  ifdef MOZ_WMF_MEDIA_ENGINE
 #    include "MFMediaEngineDecoderModule.h"
 #  endif
@@ -86,7 +87,9 @@ class PDMInitializer final {
  private:
   static void InitGpuPDMs() {
 #ifdef XP_WIN
-    WMFDecoderModule::Init();
+    if (!IsWin7AndPre2000Compatible()) {
+      WMFDecoderModule::Init();
+    }
     if (StaticPrefs::media_ffvpx_hw_enabled()) {
       FFVPXRuntimeLinker::Init();
     }
@@ -95,7 +98,9 @@ class PDMInitializer final {
 
   static void InitRddPDMs() {
 #ifdef XP_WIN
-    WMFDecoderModule::Init();
+    if (!IsWin7AndPre2000Compatible()) {
+      WMFDecoderModule::Init();
+    }
 #endif
 #ifdef MOZ_APPLEMEDIA
     AppleDecoderModule::Init();
@@ -111,11 +116,12 @@ class PDMInitializer final {
   static void InitUtilityPDMs() {
     const ipc::SandboxingKind kind = GetCurrentSandboxingKind();
 #ifdef XP_WIN
-    if (kind == ipc::SandboxingKind::UTILITY_AUDIO_DECODING_WMF) {
+    if (!IsWin7AndPre2000Compatible() &&
+        kind == ipc::SandboxingKind::UTILITY_AUDIO_DECODING_WMF) {
       WMFDecoderModule::Init();
     }
 #  ifdef MOZ_WMF_MEDIA_ENGINE
-    if (StaticPrefs::media_wmf_media_engine_enabled() &&
+    if (IsWin10OrLater() && StaticPrefs::media_wmf_media_engine_enabled() &&
         kind == ipc::SandboxingKind::MF_MEDIA_ENGINE_CDM) {
       MFMediaEngineDecoderModule::Init();
     }
@@ -142,14 +148,16 @@ class PDMInitializer final {
     if (StaticPrefs::media_allow_audio_non_utility()) {
 #endif  // !defined(MOZ_WIDGET_ANDROID)
 #ifdef XP_WIN
+      if (!IsWin7AndPre2000Compatible()) {
 #  ifdef MOZ_WMF
-      if (!StaticPrefs::media_rdd_process_enabled() ||
-          !StaticPrefs::media_rdd_wmf_enabled() ||
-          !StaticPrefs::media_utility_process_enabled() ||
-          !StaticPrefs::media_utility_wmf_enabled()) {
-        WMFDecoderModule::Init();
-      }
+        if (!StaticPrefs::media_rdd_process_enabled() ||
+            !StaticPrefs::media_rdd_wmf_enabled() ||
+            !StaticPrefs::media_utility_process_enabled() ||
+            !StaticPrefs::media_utility_wmf_enabled()) {
+          WMFDecoderModule::Init();
+        }
 #  endif
+      }
 #endif
 #ifdef MOZ_APPLEMEDIA
       AppleDecoderModule::Init();
@@ -170,7 +178,9 @@ class PDMInitializer final {
 
   static void InitDefaultPDMs() {
 #ifdef XP_WIN
-    WMFDecoderModule::Init();
+    if (!IsWin7AndPre2000Compatible()) {
+      WMFDecoderModule::Init();
+    }
 #endif
 #ifdef MOZ_APPLEMEDIA
     AppleDecoderModule::Init();
@@ -515,7 +525,7 @@ void PDMFactory::CreateGpuPDMs() {
   if (StaticPrefs::media_ffvpx_hw_enabled()) {
     StartupPDM(FFVPXRuntimeLinker::CreateDecoder());
   }
-  if (StaticPrefs::media_wmf_enabled()) {
+  if (StaticPrefs::media_wmf_enabled() && !IsWin7AndPre2000Compatible()) {
     StartupPDM(WMFDecoderModule::Create());
   }
 #endif
@@ -604,7 +614,7 @@ void PDMFactory::CreateUtilityPDMs() {
   }
 #ifdef MOZ_WMF_MEDIA_ENGINE
   if (aKind == ipc::SandboxingKind::MF_MEDIA_ENGINE_CDM) {
-    if (StaticPrefs::media_wmf_media_engine_enabled()) {
+    if (IsWin10OrLater() && StaticPrefs::media_wmf_media_engine_enabled()) {
       StartupPDM(MFMediaEngineDecoderModule::Create());
     }
   }
@@ -644,7 +654,7 @@ void PDMFactory::CreateContentPDMs() {
   if (StaticPrefs::media_allow_audio_non_utility()) {
 #endif  // !defined(MOZ_WIDGET_ANDROID)
 #ifdef XP_WIN
-    if (StaticPrefs::media_wmf_enabled()) {
+    if (StaticPrefs::media_wmf_enabled() && !IsWin7AndPre2000Compatible()) {
 #  ifdef MOZ_WMF
       if (!StaticPrefs::media_rdd_process_enabled() ||
           !StaticPrefs::media_rdd_wmf_enabled()) {
@@ -698,7 +708,7 @@ void PDMFactory::CreateContentPDMs() {
 
 void PDMFactory::CreateDefaultPDMs() {
 #ifdef XP_WIN
-  if (StaticPrefs::media_wmf_enabled()) {
+  if (StaticPrefs::media_wmf_enabled() && !IsWin7AndPre2000Compatible()) {
     if (!StartupPDM(WMFDecoderModule::Create())) {
       mFailureFlags += DecoderDoctorDiagnostics::Flags::WMFFailedToLoad;
     }

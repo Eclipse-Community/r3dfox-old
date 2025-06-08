42f7563a08b8328e7cc4dc38e8f09e295db5a645
K4sum1 <k4sum1@eclipse.cx>
2025-06-08 15:15:18 -0500

    Revert "servo: Merge #19833 - style: Remove -moz-border-*-colors (from emilio:bye-border-colors); r=xidorn"
    This reverts commit 105d76e3078f8c45ea7461f27e09b55e9a2052a9.

--- servo/components/style/properties/properties.mako.rs
+++ servo/components/style/properties/properties.mako.rs
@@ -1145,6 +1145,10 @@ impl LonghandId {
             LonghandId::BorderImageSource |
             LonghandId::BoxShadow |
             LonghandId::MaskImage |
+            LonghandId::MozBorderBottomColors |
+            LonghandId::MozBorderLeftColors |
+            LonghandId::MozBorderRightColors |
+            LonghandId::MozBorderTopColors |
             LonghandId::TextShadow
         )
         % else:

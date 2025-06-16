07eec0f920127828c0cfd1293351c72add97650e
K4sum1 <k4sum1@eclipse.cx>
2025-06-15 22:42:15 -0500

    More context menu stuff to add to view image

--- browser/base/content/browser-context.js
+++ browser/base/content/browser-context.js
@@ -160,10 +160,14 @@ document.addEventListener(
           gContextMenu.setDesktopBackground();
           break;
         case "context-bookmarkpage":
+        case "context-bookmarkpage-old":
           gContextMenu.bookmarkThisPage();
           break;
         case "context-savepage":
           gContextMenu.savePageAs();
+          break;
+        case "context-sendpage":
+          gContextMenu.sendPage();
           break;
         case "context-pocket":
           Pocket.savePage(
@@ -298,6 +302,9 @@ document.addEventListener(
           break;
         case "context-viewsource":
           BrowserCommands.viewSource(gContextMenu.browser);
+          break;
+        case "context-viewinfo":
+          gContextMenu.viewInfo();
           break;
         case "context-inspect-a11y":
           gContextMenu.inspectA11Y();

cbb5f85457ce36662d6aab2b6e47fcb94e125e9d
K4sum1 <k4sum1@eclipse.cx>
2026-04-18 07:13:11 -0700

    Revert "Bug 1790637 - Accept camelCase extra keys in JS r=janerik"
    This reverts commit 4c975bea8ba2eedd9075152e0c7c71101797dd2b.

--- toolkit/components/glean/tests/xpcshell/test_Glean.js
+++ toolkit/components/glean/tests/xpcshell/test_Glean.js
@@ -152,18 +152,6 @@ add_task(async function test_fog_event_works() {
   };
   Assert.deepEqual(expectedExtra, events[0].extra);
 
-  // camelCase extras work.
-  let extra5 = {
-    extra3LongerName: false,
-  };
-  Glean.testOnlyIpc.eventWithExtra.record(extra5);
-  events = Glean.testOnlyIpc.eventWithExtra.testGetValue();
-  Assert.equal(2, events.length, "Recorded one event too many.");
-  expectedExtra = {
-    extra3_longer_name: "false",
-  };
-  Assert.deepEqual(expectedExtra, events[1].extra);
-
   // Invalid extra keys don't crash, the event is not recorded,
   // but an error is recorded.
   let extra3 = {

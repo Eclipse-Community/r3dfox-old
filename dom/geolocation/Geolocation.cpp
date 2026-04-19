15ca127c117994db1821726711765f4a9a2f1b9d
K4sum1 <k4sum1@eclipse.cx>
2026-04-19 13:35:07 -0700

    Revert "Bug 1900225: Part 6 - Wait for Windows permission dialog to be dismissed before requesting geolocation r=emilio,win-reviewers,gstoll a=RyanVM"
    This reverts commit 5f342def317ce7bcade3b6fa7bea3f3f42616e8b.

--- dom/geolocation/Geolocation.cpp
+++ dom/geolocation/Geolocation.cpp
@@ -348,8 +348,8 @@ void Geolocation::ReallowWithSystemPermissionOrCancel(
   denyPermissionOnError.release();
 
   RefPtr<SystemGeolocationPermissionRequest> permissionRequest =
-      geolocation::RequestLocationPermissionFromUser(aBrowsingContext,
-                                                     std::move(aResolver));
+      geolocation::PresentSystemSettings(aBrowsingContext,
+                                         std::move(aResolver));
   NS_ENSURE_TRUE_VOID(permissionRequest);
 
   auto cancelRequestOnError = MakeScopeExit([&]() {
@@ -405,13 +405,12 @@ nsGeolocationRequest::Allow(JS::Handle<JS::Value> aChoices) {
     return NS_OK;
   }
 
-  if (mBehavior != SystemGeolocationPermissionBehavior::NoPrompt) {
-    // Asynchronously present the system dialog or open system preferences
-    // (RequestGeolocationPermissionFromUser will know which to do), and wait
-    // for the permission to change or the request to be canceled.  If the
-    // permission is (maybe) granted then it will call Allow again.  It actually
-    // will also re-call Allow if the permission is denied, in order to get the
-    // "denied permission" behavior.
+  if (mBehavior == SystemGeolocationPermissionBehavior::GeckoWillPromptUser) {
+    // Asynchronously present the system dialog and wait for the permission to
+    // change or the request to be canceled.  If the permission is (maybe)
+    // granted then it will call Allow again.  It actually will also re-call
+    // Allow if the permission is denied, in order to get the "denied
+    // permission" behavior.
     mBehavior = SystemGeolocationPermissionBehavior::NoPrompt;
     RefPtr<BrowsingContext> browsingContext = mWindow->GetBrowsingContext();
     if (ContentChild* cc = ContentChild::GetSingleton()) {

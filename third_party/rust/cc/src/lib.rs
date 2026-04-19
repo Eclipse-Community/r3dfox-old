b83e5afb42d7785c856d64b9cfe31dd31a3789ea
K4sum1 <k4sum1@eclipse.cx>
2026-04-19 11:37:37 -0700

    1075
    77
    
    78
    
    Revert Default to llvm-lib when using clang-cl in msvc environment
    
    Breaks compiling here.
    
    https://github.com/rust-lang/cc-rs/pull/758/changes/88fd7316cac76b3d5c94b9d550d9b68e8427a28d
    
    Update lib.rs

--- third_party/rust/cc/src/lib.rs
+++ third_party/rust/cc/src/lib.rs
@@ -2675,7 +2675,7 @@ impl Build {
                 // the -print-search-dirs option.
                 if let Some(mut cmd) = which(&compiler.path) {
                     cmd.pop();
-                    cmd.push("llvm-lib.exe");
+                    cmd.push("lib.exe");
                     if let Some(llvm_lib) = which(&cmd) {
                         lib = llvm_lib.to_str().unwrap().to_owned();
                     }

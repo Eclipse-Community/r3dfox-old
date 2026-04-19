be0181b22447b11d081e2b6d4863558c33143193
K4sum1 <k4sum1@eclipse.cx>
2026-04-19 13:35:21 -0700

    Revert "Bug 1900225: Part 5 - On Windows, do not show message about wifi scan permission if no wifi is present r=win-reviewers,necko-reviewers,valentin,gstoll a=RyanVM"
    This reverts commit 7353dad1ee3252e1dbf5212ba0adbba1339ac593.

--- netwerk/wifi/nsWifiMonitor.h
+++ netwerk/wifi/nsWifiMonitor.h
@@ -61,8 +61,6 @@ class nsWifiMonitor final : public nsIWifiMonitor, public nsIObserver {
 
   ~nsWifiMonitor();
 
-  void EnsureWifiScanner();
-
   nsresult DispatchScanToBackgroundThread(uint64_t aPollingId = 0,
                                           uint32_t aWaitMs = 0);
 

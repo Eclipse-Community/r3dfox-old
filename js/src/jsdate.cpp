a655193c394c47478b6f59edca370cffb84f09e5
K4sum1 <k4sum1@eclipse.cx>
2026-04-20 08:09:24 -0700

    Fix jsdate

--- js/src/jsdate.cpp
+++ js/src/jsdate.cpp
@@ -2845,16 +2845,19 @@ static bool FormatDate(JSContext* cx, double utcTime, FormatSpec format,
     }
   }
 
+  char gmt[10];
+  SprintfLiteral(gmt, "GMT%+.4d", offset);
+
   char buf[100];
   switch (format) {
     case FormatSpec::DateTime:
       /* Tue Oct 31 2000 09:41:40 GMT-0800 */
-      SprintfLiteral(buf, "%s %s %.2d %.4d %.2d:%.2d:%.2d GMT%+.4d",
+      SprintfLiteral(buf, "%s %s %.2d %.4d %.2d:%.2d:%.2d %s",
                      days[int(WeekDay(localTime))],
                      months[int(MonthFromTime(localTime))],
                      int(DateFromTime(localTime)), int(YearFromTime(localTime)),
                      int(HourFromTime(localTime)), int(MinFromTime(localTime)),
-                     int(SecFromTime(localTime)), offset);
+                     int(SecFromTime(localTime)), gmt);
       break;
     case FormatSpec::Date:
       /* Tue Oct 31 2000 */
@@ -2865,9 +2868,9 @@ static bool FormatDate(JSContext* cx, double utcTime, FormatSpec format,
       break;
     case FormatSpec::Time:
       /* 09:41:40 GMT-0800 */
-      SprintfLiteral(buf, "%.2d:%.2d:%.2d GMT%+.4d",
+      SprintfLiteral(buf, "%.2d:%.2d:%.2d %s",
                      int(HourFromTime(localTime)), int(MinFromTime(localTime)),
-                     int(SecFromTime(localTime)), offset);
+                     int(SecFromTime(localTime)), gmt);
       break;
   }
 
@@ -2879,16 +2882,21 @@ static bool FormatDate(JSContext* cx, double utcTime, FormatSpec format,
   // Append the time zone string if present.
   if (timeZoneComment && !timeZoneComment->empty()) {
     str = js::ConcatStrings<CanGC>(cx, str, timeZoneComment);
-    if (!str) {
-      return false;
-    }
+  } else {
+    SprintfLiteral(buf, " (%s)", gmt);
+    RootedString str2(cx, NewStringCopyZ<CanGC>(cx, buf));
+    str = js::ConcatStrings<CanGC>(cx, str, str2);
+  }
+
+  if (!str) {
+    return false;
   }
 
   rval.setString(str);
   return true;
 }
 
-#if !EXPOSE_INTL_API
+#if !EXPOSE_INTL_API /* replace with JS_HAS_INTL_API when removed */
 static bool ToLocaleFormatHelper(JSContext* cx, HandleObject obj,
                                  const char* format, MutableHandleValue rval) {
   double utcTime = obj->as<DateObject>().UTCTime().toNumber();
@@ -3135,7 +3143,7 @@ static const JSFunctionSpec date_methods[] = {
     JS_FN("setMilliseconds", date_setMilliseconds, 1, 0),
     JS_FN("setUTCMilliseconds", date_setUTCMilliseconds, 1, 0),
     JS_FN("toUTCString", date_toGMTString, 0, 0),
-#if EXPOSE_INTL_API
+#if EXPOSE_INTL_API /* replace with JS_HAS_INTL_API when removed */
     JS_SELF_HOSTED_FN(js_toLocaleString_str, "Date_toLocaleString", 0, 0),
     JS_SELF_HOSTED_FN("toLocaleDateString", "Date_toLocaleDateString", 0, 0),
     JS_SELF_HOSTED_FN("toLocaleTimeString", "Date_toLocaleTimeString", 0, 0),

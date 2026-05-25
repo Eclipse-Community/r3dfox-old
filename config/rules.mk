a0a8e4eb0deec07e40cabf6402cb62b03ca4f5af
K4sum1 <k4sum1@eclipse.cx>
2026-05-25 06:20:39 -0700

    applied https://github.com/freebsd/freebsd-ports/blob/7d67a39637dc/www/firefox/files/patch-rust-option to restore --disable-rust option

--- config/rules.mk
+++ config/rules.mk
@@ -838,6 +838,7 @@ endif
 
 $(foreach file,$(DUMP_SYMS_TARGETS),$(eval $(call syms_template,$(file),$(file)_syms.track)))
 
+ifdef MOZ_RUST
 cargo_host_flag := --target=$(RUST_HOST_TARGET)
 cargo_target_flag := --target=$(RUST_TARGET)
 
@@ -1055,6 +1056,7 @@ else
 force-cargo-host-program-check:
 	@true
 endif # HOST_RUST_PROGRAMS
+endif # MOZ_RUST
 
 $(SOBJS):
 	$(REPORT_BUILD)
--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -231,11 +231,13 @@
 extern uint32_t gRestartMode;
 extern void InstallSignalHandlers(const char *ProgramName);
 
+#ifdef MOZ_RUST
 // This workaround is fixed in Rust 1.19. For details, see bug 1358151.
 // Implementation in toolkit/library/rust/shared/lib.rs
 extern "C" {
   void rust_init_please_remove_this_after_updating_rust_1_19();
 }
+#endif
 
 #define FILE_COMPATIBILITY_INFO NS_LITERAL_CSTRING("compatibility.ini")
 #define FILE_INVALIDATE_CACHES NS_LITERAL_CSTRING(".purgecaches")
@@ -3149,8 +3151,10 @@ XREMain::XRE_mainInit(bool* aExitFlag)
     return 1;
   *aExitFlag = false;
 
+#ifdef MOZ_RUST
   // This workaround is fixed in Rust 1.19. For details, see bug 1358151.
   rust_init_please_remove_this_after_updating_rust_1_19();
+#endif
 
   atexit(UnexpectedExit);
   auto expectedShutdown = mozilla::MakeScopeExit([&] {

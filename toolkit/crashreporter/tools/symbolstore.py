447665a66ec0897fa45e4a979511382dd8dd7b57
K4sum1 <k4sum1@eclipse.cx>
2026-05-25 07:31:51 -0700

    Revert "bug 1275424 - hardcode Rust source paths in symbolstore.py. r=gps"
    This reverts commit e9db7838e4a4b87dbd0323694ce7a32842dc6fa8.

--- toolkit/crashreporter/tools/symbolstore.py
+++ toolkit/crashreporter/tools/symbolstore.py
@@ -394,20 +394,6 @@ class Dumper:
         if repo_manifest:
             self.parse_repo_manifest(repo_manifest)
         self.file_mapping = file_mapping or {}
-        # Add a static mapping for Rust sources.
-        target_os = buildconfig.substs['OS_ARCH']
-        rust_srcdir = None
-        if target_os == 'WINNT':
-            rust_srcdir = 'C:/projects/rust/'
-        elif target_os == 'Darwin':
-            rust_srcdir = '/Users/travis/build/rust-lang/rust/'
-        elif target_os == 'Linux':
-            rust_srcdir = '/checkout/'
-        if rust_srcdir is not None:
-            self.srcdirs.append(rust_srcdir)
-            Dumper.srcdirRepoInfo[rust_srcdir] = GitRepoInfo(rust_srcdir,
-                                                             buildconfig.substs['RUSTC_COMMIT'],
-                                                             'https://github.com/rust-lang/rust/')
 
     def parse_repo_manifest(self, repo_manifest):
         """

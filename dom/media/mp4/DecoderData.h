3d35f0df50bf5dcee9e2a765b165a63c6b998aab
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 14:32:18 -0700

    Revert "Bug 1417795 - remove mp4_demuxer namespace part4. r=kinetik"
    This reverts commit 80dffd02b7685a0b783e6e21e78e4a345b430385.

--- dom/media/mp4/DecoderData.h
+++ dom/media/mp4/DecoderData.h
@@ -20,7 +20,7 @@ typedef struct mp4parse_track_audio_info mp4parse_track_audio_info;
 typedef struct mp4parse_track_video_info mp4parse_track_video_info;
 }
 
-namespace mozilla
+namespace mp4_demuxer
 {
 
 class MP4Demuxer;

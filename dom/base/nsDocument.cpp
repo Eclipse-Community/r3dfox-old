35a78610ff1867a2d77a062485d118337671357e
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 09:07:18 -0700

    Revert "Bug 1354989 - Avoid pivoting via UTF-16 when loading CSS in the Stylo mode. r=jdm,SimonSapin"
    This reverts commit 3c8567b60d5ae275eb2722a44f116653342c6e90.

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -9756,8 +9756,7 @@ NS_IMPL_ISUPPORTS(StubCSSLoaderObserver, nsICSSLoaderObserver)
 } // namespace
 
 void
-nsDocument::PreloadStyle(nsIURI* uri,
-                         const Encoding* aEncoding,
+nsDocument::PreloadStyle(nsIURI* uri, const nsAString& charset,
                          const nsAString& aCrossOriginAttr,
                          const ReferrerPolicy aReferrerPolicy,
                          const nsAString& aIntegrity)
@@ -9766,14 +9765,11 @@ nsDocument::PreloadStyle(nsIURI* uri,
   nsCOMPtr<nsICSSLoaderObserver> obs = new StubCSSLoaderObserver();
 
   // Charset names are always ASCII.
-  CSSLoader()->LoadSheet(uri,
-                         true,
-                         NodePrincipal(),
-                         aEncoding,
+  CSSLoader()->LoadSheet(uri, true, NodePrincipal(),
+                         NS_LossyConvertUTF16toASCII(charset),
                          obs,
                          Element::StringToCORSMode(aCrossOriginAttr),
-                         aReferrerPolicy,
-                         aIntegrity);
+                         aReferrerPolicy, aIntegrity);
 }
 
 nsresult
--- parser/html/nsHtml5TreeOpExecutor.cpp
+++ parser/html/nsHtml5TreeOpExecutor.cpp
@@ -1028,10 +1028,7 @@ nsHtml5TreeOpExecutor::PreloadStyle(const nsAString& aURL,
     referrerPolicy = styleReferrerPolicy;
   }
 
-  mDocument->PreloadStyle(uri,
-                          Encoding::ForLabel(aCharset),
-                          aCrossOrigin,
-                          referrerPolicy,
+  mDocument->PreloadStyle(uri, aCharset, aCrossOrigin, referrerPolicy,
                           aIntegrity);
 }
 

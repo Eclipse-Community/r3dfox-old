c2b8841fffca53b2f072aa98fcfb115937ae71cf
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 14:33:52 -0700

    Revert "Bug 1417795 - remove mp4_demuxer namespace part1. r=kinetik"
    This reverts commit a2cd247c8f8a188977161e5e03fe763f37d6520d.

--- dom/media/mp4/Box.h
+++ dom/media/mp4/Box.h
@@ -12,7 +12,7 @@
 #include "MediaResource.h"
 #include "mozilla/EndianUtils.h"
 #include "mp4_demuxer/AtomType.h"
-#include "BufferReader.h"
+#include "mp4_demuxer/BufferReader.h"
 
 using namespace mozilla;
 

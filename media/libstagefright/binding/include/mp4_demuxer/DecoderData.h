0ce6d00dc8f638335b513f841da6801deb468a39
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 14:37:42 -0700

    Start building libstagefright again + includes and code for it
    Revert "Bug 1415809 - stop building stagefright. r=kinetik"
    
    This reverts commit 776dcceab45b142c3862dabe8d7a89fd48ab5475.
    
    Revert "Bug 1415809 - remove MP4MetadataStagefright. r=kinetik"
    
    This reverts commit ff70c5cd00f07d804c5b48130be1235f18123a55.
    
    Revert "Bug 1418868 - remove IndiceWrapper abstract layer. r=kinetik"
    
    This reverts commit 3202740dff52ae4bbc88d877ddb382ebaebb452d.
    
    Revert "Bug 1415809 - rename MP4MetadataRust to MP4Metadata. r=kinetik"
    
    This reverts commit f2716d56933155496cfd321f6519034d27612408.
    
    Revert "Bug 1415809 - remove stagefright stuff from DecoderData. r=kinetik"
    
    This reverts commit 13cd8ce149aa731b9e7be65ff1248686553638b5.
    
    Revert "Bug 1415809 - update gtest. r=kinetik"
    
    This reverts commit 59da86b6b1e118e73f6abd1dc8dc8485957b32dd.

--- media/libstagefright/binding/include/mp4_demuxer/DecoderData.h
+++ media/libstagefright/binding/include/mp4_demuxer/DecoderData.h
@@ -5,6 +5,7 @@
 #ifndef DECODER_DATA_H_
 #define DECODER_DATA_H_
 
+#include "MediaData.h"
 #include "MediaInfo.h"
 #include "mozilla/RefPtr.h"
 #include "mozilla/Result.h"
@@ -13,7 +14,11 @@
 #include "nsString.h"
 #include "nsTArray.h"
 #include "nsString.h"
-#include "mp4parse.h"
+
+namespace stagefright
+{
+class MetaData;
+}
 
 namespace mp4_demuxer
 {
@@ -58,6 +63,9 @@ class MP4AudioInfo : public mozilla::AudioInfo
 public:
   MP4AudioInfo() = default;
 
+  void Update(const stagefright::MetaData* aMetaData,
+              const char* aMimeType);
+
   void Update(const Mp4parseTrackInfo* track,
               const Mp4parseTrackAudioInfo* audio);
 
@@ -68,6 +76,9 @@ class MP4VideoInfo : public mozilla::VideoInfo
 {
 public:
   MP4VideoInfo() = default;
+
+  void Update(const stagefright::MetaData* aMetaData,
+              const char* aMimeType);
 
   void Update(const Mp4parseTrackInfo* track,
               const Mp4parseTrackVideoInfo* video);

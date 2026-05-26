03481c7e6fa0fded77e56c5bcf892ed1182d7b6f
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 16:56:31 -0700

    Update nsDocShell.cpp

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -2191,11 +2191,7 @@ nsDocShell::SetForcedCharset(const nsACString& aCharset)
 NS_IMETHODIMP
 nsDocShell::GetForcedCharset(nsACString& aResult)
 {
-  if (mForcedCharset) {
-    aResult = mForcedCharset;
-  } else {
-    aResult.Truncate();
-  }
+  aResult = mForcedCharset;
   return NS_OK;
 }
 

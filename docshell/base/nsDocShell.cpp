e7ca797139773ae45d3fd867e762a5295c294c4a
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 08:36:25 -0700

    Switch away from nsXPIDL(C)String to fix uconv
    Switch away from nsXPIDL(C)String to fix uconv part 2
    
    Update nsIndexedToHTML.cpp

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -11308,7 +11308,7 @@ nsDocShell::ScrollToAnchor(bool aCurHasRef, bool aNewHasRef,
       NS_ENSURE_SUCCESS(rv, rv);
 
       // Unescape and convert to unicode
-      nsXPIDLString uStr;
+      nsAutoString uStr;
 
       rv = textToSubURI->UnEscapeAndConvert(PromiseFlatCString(aCharset).get(),
                                             PromiseFlatCString(aNewHash).get(),

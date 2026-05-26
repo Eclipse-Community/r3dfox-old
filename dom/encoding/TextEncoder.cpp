32032d02dcda15ff8c2de35acb708fc86ff9e812
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 03:16:06 -0700

    Revert "Bug 1261841 part 2 - Use encoding_rs instead of uconv. r=emk,mystor."
    This reverts commit 432653453a8facb744e5239be94c9043128259cb.
    
    Update BodyExtractor.cpp
    
    Update TextDecoder.cpp

--- dom/encoding/TextEncoder.cpp
+++ dom/encoding/TextEncoder.cpp
@@ -5,7 +5,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/dom/TextEncoder.h"
-#include "mozilla/Encoding.h"
+#include "mozilla/dom/EncodingUtils.h"
+#include "mozilla/UniquePtrExtensions.h"
+#include "nsContentUtils.h"
 
 namespace mozilla {
 namespace dom {
@@ -13,6 +15,8 @@ namespace dom {
 void
 TextEncoder::Init()
 {
+  // Create an encoder object for utf-8.
+  mEncoder = EncodingUtils::EncoderForEncoding(NS_LITERAL_CSTRING("UTF-8"));
 }
 
 void
@@ -22,23 +26,49 @@ TextEncoder::Encode(JSContext* aCx,
                     JS::MutableHandle<JSObject*> aRetval,
                     ErrorResult& aRv)
 {
-  nsAutoCString utf8;
-  nsresult rv;
-  const Encoding* ignored;
-  Tie(rv, ignored) = UTF_8_ENCODING->Encode(aString, utf8);
+  // Run the steps of the encoding algorithm.
+  int32_t srcLen = aString.Length();
+  int32_t maxLen;
+  const char16_t* data = aString.BeginReading();
+  nsresult rv = mEncoder->GetMaxLength(data, srcLen, &maxLen);
   if (NS_FAILED(rv)) {
     aRv.Throw(rv);
     return;
   }
-
-  JSAutoCompartment ac(aCx, aObj);
-  JSObject* outView = Uint8Array::Create(
-    aCx, utf8.Length(), reinterpret_cast<const uint8_t*>(utf8.BeginReading()));
-  if (!outView) {
+  // Need a fallible allocator because the caller may be a content
+  // and the content can specify the length of the string.
+  auto buf = MakeUniqueFallible<char[]>(maxLen + 1);
+  if (!buf) {
     aRv.Throw(NS_ERROR_OUT_OF_MEMORY);
     return;
   }
 
+  int32_t dstLen = maxLen;
+  rv = mEncoder->Convert(data, &srcLen, buf.get(), &dstLen);
+
+  // Now reset the encoding algorithm state to the default values for encoding.
+  int32_t finishLen = maxLen - dstLen;
+  rv = mEncoder->Finish(&buf[dstLen], &finishLen);
+  if (NS_SUCCEEDED(rv)) {
+    dstLen += finishLen;
+  }
+
+  JSObject* outView = nullptr;
+  if (NS_SUCCEEDED(rv)) {
+    buf[dstLen] = '\0';
+    JSAutoCompartment ac(aCx, aObj);
+    outView = Uint8Array::Create(aCx, dstLen,
+                                 reinterpret_cast<uint8_t*>(buf.get()));
+    if (!outView) {
+      aRv.Throw(NS_ERROR_OUT_OF_MEMORY);
+      return;
+    }
+  }
+
+  if (NS_FAILED(rv)) {
+    aRv.Throw(rv);
+    return;
+  }
   aRetval.set(outView);
 }
 
--- extensions/spellcheck/hunspell/glue/mozHunspell.cpp
+++ extensions/spellcheck/hunspell/glue/mozHunspell.cpp
@@ -74,10 +74,11 @@
 #include <stdlib.h>
 #include "nsIPrefService.h"
 #include "nsIPrefBranch.h"
+#include "mozilla/dom/EncodingUtils.h"
 #include "mozilla/dom/ContentParent.h"
 
 using mozilla::dom::ContentParent;
-using namespace mozilla;
+using mozilla::dom::EncodingUtils;
 
 NS_IMPL_CYCLE_COLLECTING_ADDREF(mozHunspell)
 NS_IMPL_CYCLE_COLLECTING_RELEASE(mozHunspell)
@@ -91,7 +92,10 @@ NS_INTERFACE_MAP_BEGIN(mozHunspell)
   NS_INTERFACE_MAP_ENTRIES_CYCLE_COLLECTION(mozHunspell)
 NS_INTERFACE_MAP_END
 
-NS_IMPL_CYCLE_COLLECTION(mozHunspell, mPersonalDictionary)
+NS_IMPL_CYCLE_COLLECTION(mozHunspell,
+                         mPersonalDictionary,
+                         mEncoder,
+                         mDecoder)
 
 template<> mozilla::Atomic<size_t> mozilla::CountingAllocatorBase<HunspellAllocator>::sAmount(0);
 
@@ -200,13 +204,16 @@ NS_IMETHODIMP mozHunspell::SetDictionary(const char16_t *aDictionary)
   if (!mHunspell)
     return NS_ERROR_OUT_OF_MEMORY;
 
-  auto encoding =
-    Encoding::ForLabelNoReplacement(mHunspell->get_dict_encoding());
-  if (!encoding) {
+  nsAutoCString label(mHunspell->get_dict_encoding().c_str());
+  nsAutoCString encoding;
+  if (!EncodingUtils::FindEncodingForLabelNoReplacement(label, encoding)) {
     return NS_ERROR_UCONV_NOCONV;
   }
-  mEncoder = encoding->NewEncoder();
-  mDecoder = encoding->NewDecoderWithoutBOMHandling();
+  mEncoder = EncodingUtils::EncoderForEncoding(encoding);
+  mDecoder = EncodingUtils::DecoderForEncoding(encoding);
+
+  if (mEncoder)
+    mEncoder->SetOutputErrorBehavior(mEncoder->kOnError_Signal, nullptr, '?');
 
   int32_t pos = mDictionary.FindChar('-');
   if (pos == -1)
@@ -483,31 +490,20 @@ mozHunspell::ConvertCharset(const char16_t* aStr, std::string* aDst)
   NS_ENSURE_ARG_POINTER(aDst);
   NS_ENSURE_TRUE(mEncoder, NS_ERROR_NULL_POINTER);
 
-  auto src = MakeStringSpan(aStr);
-  CheckedInt<size_t> needed =
-    mEncoder->MaxBufferLengthFromUTF16WithoutReplacement(src.Length());
-  if (!needed.isValid()) {
-    return NS_ERROR_OUT_OF_MEMORY;
+  int32_t outLength;
+  int32_t inLength = NS_strlen(aStr);
+  nsresult rv = mEncoder->GetMaxLength(aStr, inLength, &outLength);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  aDst->resize(outLength);
+
+  char* dst = &aDst->operator[](0);
+  rv = mEncoder->Convert(aStr, &inLength, dst, &outLength);
+  if (NS_SUCCEEDED(rv)) {
+    aDst->resize(outLength);
   }
 
-  aDst->resize(needed.value());
-
-  char* dstPtr = &aDst->operator[](0);
-  auto dst = MakeSpan(reinterpret_cast<uint8_t*>(dstPtr), needed.value());
-
-  uint32_t result;
-  size_t read;
-  size_t written;
-  Tie(result, read, written) =
-    mEncoder->EncodeFromUTF16WithoutReplacement(src, dst, true);
-  Unused << read;
-  MOZ_ASSERT(result != kOutputFull);
-  if (result != kInputEmpty) {
-    return NS_ERROR_UENC_NOMAPPING;
-  }
-  aDst->resize(written);
-  mEncoder->Encoding()->NewEncoderInto(*mEncoder);
-  return NS_OK;
+  return rv;
 }
 
 NS_IMETHODIMP
@@ -561,42 +557,22 @@ NS_IMETHODIMP mozHunspell::Suggest(const char16_t *aWord, char16_t ***aSuggestio
     if (*aSuggestions) {
       uint32_t index = 0;
       for (index = 0; index < *aSuggestionCount && NS_SUCCEEDED(rv); ++index) {
-        // If the IDL used an array of AString, we could use
-        // Encoding::DecodeWithoutBOMHandling() here.
         // Convert the suggestion to utf16
-        Span<const char> charSrc(suggestions[index]);
-        auto src = AsBytes(charSrc);
-        CheckedInt<size_t> needed =
-          mDecoder->MaxUTF16BufferLength(src.Length());
-        if (!needed.isValid()) {
-          rv = NS_ERROR_OUT_OF_MEMORY;
-          break;
+        int32_t inLength = suggestions[index].size();
+        int32_t outLength;
+        rv = mDecoder->GetMaxLength(suggestions[index].c_str(), inLength, &outLength);
+        if (NS_SUCCEEDED(rv))
+        {
+          (*aSuggestions)[index] = (char16_t *) moz_xmalloc(sizeof(char16_t) * (outLength+1));
+          if ((*aSuggestions)[index])
+          {
+            rv = mDecoder->Convert(suggestions[index].c_str(), &inLength, (*aSuggestions)[index], &outLength);
+            if (NS_SUCCEEDED(rv))
+              (*aSuggestions)[index][outLength] = 0;
+          }
+          else
+            rv = NS_ERROR_OUT_OF_MEMORY;
         }
-        size_t dstLen = needed.value();
-        needed += 1;
-        needed *= sizeof(char16_t);
-        if (!needed.isValid()) {
-          rv = NS_ERROR_OUT_OF_MEMORY;
-          break;
-        }
-        (*aSuggestions)[index] = (char16_t*)moz_xmalloc(needed.value());
-        if (!((*aSuggestions)[index])) {
-          rv = NS_ERROR_OUT_OF_MEMORY;
-          break;
-        }
-        auto dst = MakeSpan((*aSuggestions)[index], dstLen);
-        uint32_t result;
-        size_t read;
-        size_t written;
-        bool hadErrors;
-        Tie(result, read, written, hadErrors) =
-          mDecoder->DecodeToUTF16(src, dst, true);
-        MOZ_ASSERT(result == kInputEmpty);
-        MOZ_ASSERT(read == src.Length());
-        MOZ_ASSERT(written <= dstLen);
-        Unused << hadErrors;
-        (*aSuggestions)[index][written] = 0;
-        mDecoder->Encoding()->NewDecoderWithoutBOMHandlingInto(*mDecoder);
       }
 
       if (NS_FAILED(rv))

d3da9fafd984e2e93b5bdc3cc1ca3f30877b0200
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 03:14:10 -0700

    Revert "Bug 1369020 - Remove nsContentUtils::ConvertStringFromEncoding and nsContentUtils::CheckForBOM. r=hsivonen"
    This reverts commit 9865b2fdebeaf599dcc31a84aebd899c9569cc4b.

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -175,7 +175,7 @@
 
 #include "mozAutoDocUpdate.h"
 #include "nsGlobalWindow.h"
-#include "mozilla/Encoding.h"
+#include "mozilla/dom/EncodingUtils.h"
 #include "nsDOMNavigationTiming.h"
 
 #include "nsSMILAnimationController.h"
@@ -4011,9 +4011,9 @@ nsDocument::TryChannelCharset(nsIChannel *aChannel,
     nsAutoCString charsetVal;
     nsresult rv = aChannel->GetContentCharset(charsetVal);
     if (NS_SUCCEEDED(rv)) {
-      const Encoding* preferred = Encoding::ForLabel(charsetVal);
-      if (preferred) {
-        preferred->Name(aCharset);
+      nsAutoCString preferred;
+      if(EncodingUtils::FindEncodingForLabel(charsetVal, preferred)) {
+        aCharset = preferred;
         aCharsetSource = kCharsetFromChannel;
         return;
       } else if (aExecutor && !charsetVal.IsEmpty()) {
@@ -10175,9 +10175,10 @@ nsDocument::ScrollToRef()
 
     if (NS_FAILED(rv)) {
       const nsACString &docCharset = GetDocumentCharacterSet();
-      const Encoding* encoding = Encoding::ForName(docCharset);
 
-      rv = encoding->DecodeWithoutBOMHandling(unescapedRef, ref);
+      rv = nsContentUtils::ConvertStringFromEncoding(docCharset,
+                                                     unescapedRef,
+                                                     ref);
 
       if (NS_SUCCEEDED(rv) && !ref.IsEmpty()) {
         rv = shell->GoToAnchor(ref, mChangeScrollPosWhenScrollingToRef);

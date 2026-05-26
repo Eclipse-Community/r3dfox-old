cf5b8889aac5b9089ffa7d4b69acee4abbfc1cfe
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 14:32:07 -0700

    Revert "Bug 1419627 - Update mp4parse-rust callers in cheddar->cbindgen migration.  r=rillian"
    This reverts commit 169b8f690658e958539b1eef3369d815161b21d3.
    
    Revert "Bug 1418868 - remove IndiceWrapper abstract layer. r=kinetik"
    
    This reverts commit 3202740dff52ae4bbc88d877ddb382ebaebb452d.
    
    Revert "Bug 1415809 - remove MP4MetadataStagefright. r=kinetik"
    
    This reverts commit ff70c5cd00f07d804c5b48130be1235f18123a55.
    
    Revert "Bug 1415809 - return error when parsing fails. r=kinetik"
    
    This reverts commit 77f0ffd196361877727c3604a73d8533dce99207.
    
    Revert "Bug 1415809 - rename MP4MetadataRust to MP4Metadata. r=kinetik"
    
    This reverts commit f2716d56933155496cfd321f6519034d27612408.
    
    Revert "Bug 1415809 - remove stagefright stuff from DecoderData. r=kinetik"
    
    This reverts commit 13cd8ce149aa731b9e7be65ff1248686553638b5.
    
    Revert "Bug 1415809 - update gtest. r=kinetik"
    
    This reverts commit 59da86b6b1e118e73f6abd1dc8dc8485957b32dd.
    
    Revert "Bug 1415809 - stop building stagefright. r=kinetik"
    
    This reverts commit 776dcceab45b142c3862dabe8d7a89fd48ab5475.
    
    Revert "Bug 1417011 - rename fmp4 to mp4. r=kinetik"
    
    This reverts commit a60c99b5358840a441fe51850c8080bca1729ea5.
    
    Revert "Bug 1417011 - move rust mp4 parser to media/mp4parse-rust. r=kinetik"
    
    This reverts commit 20418e06c4b01c3f00986e23019f772be4672bc9.
    
    Revert "Bug 1417011 - move BufferReader, BitReader and ByteWriter to dom/media. r=kinetik"
    
    This reverts commit 23ba3bcec88e531aa684ad50c56753a526758819.
    
    Revert "Bug 1417011 - move Adts.h, AnnexB.h and H264.h to agnostic/mp4_demuxer. r=kinetik"
    
    This reverts commit 0c947765e53b96ce3f65fe909692e9577cfea5fb.
    
    Revert "Bug 1417011 - move MP4Metadata and MoofParser to ./dom/media/mp4. r=kinetik"
    
    This reverts commit b3c4d27b099e288a777c10e8687eb4a3c04421df.
    
    Revert "Bug 1417794 - move Adts.h, AnnexB.h and H264.h to dom/media/platform/agnostic/bytestreams. r=kinetik"
    
    This reverts commit 187142dbec58f0009b92e13668be6f10ac921962.
    
    Revert "Bug 1417011 - move mp4 gtest. r=kinetik"
    
    This reverts commit 4b077a3d5d97e053ab43973e1b1cb81dbadd686d.
    
    Revert "Bug 1417011 - remove stagefright folder. r=kinetik"
    
    This reverts commit 7bdbfe6743374919d5d1f9ecf7c44e00be432ed0.
    
    Revert "Bug 1417795 - remove mp4_demuxer namespace part1. r=kinetik"
    
    This reverts commit a2cd247c8f8a188977161e5e03fe763f37d6520d.
    
    Revert "Bug 1417795 - remove mp4_demuxer namespace part2. r=kinetik"
    
    This reverts commit a0056437d9a112fef91216d694254a88a92655d6.
    
    Revert "Bug 1417795 - remove mp4_demuxer namespace part3. r=kinetik"
    
    This reverts commit d6a0eb9d74e00358a923ec2261396c1f58709ee9.
    
    Revert "Bug 1417795 - remove mp4_demuxer namespace part4. r=kinetik"
    
    This reverts commit 80dffd02b7685a0b783e6e21e78e4a345b430385.
    
    Revert "Bug 1417795 - remove mp4_demuxer namespace part5. r=kinetik"
    
    This reverts commit be0c9536ec8c8c0ad113c667839e6331f983d4bf.
    
    Revert "Bug 1417795 - remove mp4_demuxer namespace part6. r=kinetik"
    
    This reverts commit 1691dbc3fd502bfaa51ab641afd8c6acd14e458a.
    
    Revert "Bug 1417791 - correct the header path in rust parser updating script. r=kinetik"
    
    This reverts commit 818f7e1ab66d786f18b75c329df84798352a2195.

--- dom/media/gmp/ChromiumCDMParent.cpp
+++ dom/media/gmp/ChromiumCDMParent.cpp
@@ -17,8 +17,8 @@
 #include "mozilla/dom/MediaKeyMessageEventBinding.h"
 #include "mozilla/gmp/GMPTypes.h"
 #include "mozilla/Unused.h"
-#include "AnnexB.h"
-#include "H264.h"
+#include "mp4_demuxer/AnnexB.h"
+#include "mp4_demuxer/H264.h"
 
 #define NS_DispatchToMainThread(...) CompileError_UseAbstractMainThreadInstead
 
@@ -917,8 +917,8 @@ ChromiumCDMParent::InitializeVideoDecoder(
 
   mMaxRefFrames =
     (aConfig.mCodec() == cdm::VideoDecoderConfig::kCodecH264)
-      ? H264::HasSPS(aInfo.mExtraData)
-          ? H264::ComputeMaxRefFrames(aInfo.mExtraData)
+      ? mp4_demuxer::H264::HasSPS(aInfo.mExtraData)
+          ? mp4_demuxer::H264::ComputeMaxRefFrames(aInfo.mExtraData)
           : 16
       : 0;
 

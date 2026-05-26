ef4d19f29eb4f5ff884e627fd62cb5a79f65004b
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 09:07:18 -0700

    Revert "Bug 1354989 - Avoid pivoting via UTF-16 when loading CSS in the Stylo mode. r=jdm,SimonSapin"
    This reverts commit 3c8567b60d5ae275eb2722a44f116653342c6e90.

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -10033,8 +10033,7 @@ NS_IMPL_ISUPPORTS(StubCSSLoaderObserver, nsICSSLoaderObserver)
 } // namespace
 
 void
-nsDocument::PreloadStyle(nsIURI* uri,
-                         const Encoding* aEncoding,
+nsDocument::PreloadStyle(nsIURI* uri, const nsAString& charset,
                          const nsAString& aCrossOriginAttr,
                          const ReferrerPolicy aReferrerPolicy,
                          const nsAString& aIntegrity)
@@ -10043,14 +10042,11 @@ nsDocument::PreloadStyle(nsIURI* uri,
   nsCOMPtr<nsICSSLoaderObserver> obs = new StubCSSLoaderObserver();
 
   // Charset names are always ASCII.
-  CSSLoader()->LoadSheet(uri,
-                         true,
-                         NodePrincipal(),
-                         aEncoding,
+  CSSLoader()->LoadSheet(uri, true, NodePrincipal(),
+                         NS_LossyConvertUTF16toASCII(charset),
                          obs,
                          Element::StringToCORSMode(aCrossOriginAttr),
-                         aReferrerPolicy,
-                         aIntegrity);
+                         aReferrerPolicy, aIntegrity);
 }
 
 nsresult

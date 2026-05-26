373fd79e9a8e4eba69b5b0ce3af985f648cbeef4
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 04:55:04 -0700

    Revert "Bug 1373984 - Turn nsIDocument::mCharacterSet into mozilla::NotNull<const mozilla::Encoding*>. r=hsivonen"
    This reverts commit b515c9c804225d19ec62b35a8ddf739872783d43.
    
    i fukd up add to Revert "Bug 1373984 - Turn nsIDocument::mCharacterSet into mozilla::NotNull<const mozilla::Encoding*>. r=hsivonen"
    
    Add to Revert "Bug 1373984 - Turn nsIDocument::mCharacterSet into mozilla::NotNull<const mozilla::Encoding*>. r=hsivonen"

--- parser/xml/nsSAXXMLReader.h
+++ parser/xml/nsSAXXMLReader.h
@@ -16,7 +16,6 @@
 #include "nsISAXErrorHandler.h"
 #include "nsCycleCollectionParticipant.h"
 #include "mozilla/Attributes.h"
-#include "mozilla/NotNull.h"
 
 #define NS_SAXXMLREADER_CONTRACTID "@mozilla.org/saxparser/xmlreader;1"
 #define NS_SAXXMLREADER_CID  \
@@ -61,8 +60,9 @@ public:
   {
   }
   
-  virtual void SetDocumentCharset(NotNull<const Encoding*> aEncoding) override
+  NS_IMETHOD SetDocumentCharset(nsACString& aCharset) override
   {
+    return NS_OK;
   }
   
   virtual nsISupports *GetTarget() override
@@ -80,8 +80,8 @@ private:
   nsCOMPtr<nsIRequestObserver> mParserObserver;
   bool mIsAsyncParse;
   static bool TryChannelCharset(nsIChannel *aChannel,
-                                int32_t& aCharsetSource,
-                                NotNull<const Encoding*>& aEncoding);
+                                  int32_t& aCharsetSource,
+                                  nsACString& aCharset);
   nsresult EnsureBaseURI();
   nsresult InitParser(nsIRequestObserver *aListener, nsIChannel *aChannel);
   nsresult SplitExpatName(const char16_t *aExpatName,

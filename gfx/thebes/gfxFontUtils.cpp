ff3d915ebb6cd8ca6eace41cb363c748ca6a97ff
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 03:16:06 -0700

    Revert "Bug 1261841 part 2 - Use encoding_rs instead of uconv. r=emk,mystor."
    This reverts commit 432653453a8facb744e5239be94c9043128259cb.
    
    Update BodyExtractor.cpp
    
    Update TextDecoder.cpp
    
    Remove unnecessicary mozilla/Encoding.h mentions

--- gfx/thebes/gfxFontUtils.cpp
+++ gfx/thebes/gfxFontUtils.cpp
@@ -18,7 +18,7 @@
 
 #include "nsCOMPtr.h"
 #include "nsIUUIDGenerator.h"
-#include "mozilla/Encoding.h"
+#include "nsIUnicodeDecoder.h"
 
 #include "harfbuzz/hb.h"
 
@@ -1421,16 +1421,6 @@ gfxFontUtils::GetCharsetForFontName(uint16_t aPlatform, uint16_t aScript, uint16
     return nullptr;
 }
 
-template<int N>
-static bool
-StartsWith(const nsACString& string, const char (&prefix)[N])
-{
-  if (N - 1 > string.Length()) {
-    return false;
-  }
-  return memcmp(string.Data(), prefix, N - 1) == 0;
-}
-
 // convert a raw name from the name table to an nsString, if possible;
 // return value indicates whether conversion succeeded
 bool
@@ -1472,12 +1462,12 @@ gfxFontUtils::DecodeFontName(const char *aNameData, int32_t aByteLen,
         return true;
     }
 
-    nsDependentCString encodingName(csName);
-    if (StartsWith(encodingName, "x-mac-") &&
-        !encodingName.EqualsLiteral("x-mac-cyrillic")) {
+    nsCOMPtr<nsIUnicodeDecoder> decoder =
+        mozilla::dom::EncodingUtils::DecoderForEncoding(csName);
+    if (!decoder) {
 #ifdef XP_MACOSX
         // Special case for macOS only: support legacy Mac encodings
-        // that aren't part of the Encoding Standard.
+        // that DecoderForEncoding didn't handle.
         if (aPlatformCode == PLATFORM_ID_MAC) {
             CFStringRef str =
                 CFStringCreateWithBytes(kCFAllocatorDefault,
@@ -1497,10 +1487,24 @@ gfxFontUtils::DecodeFontName(const char *aNameData, int32_t aByteLen,
         return false;
     }
 
-    auto encoding = Encoding::ForName(encodingName);
-    auto rv = encoding->DecodeWithoutBOMHandling(
-      AsBytes(MakeSpan(aNameData, aByteLen)), aName);
-    return NS_SUCCEEDED(rv);
+    int32_t destLength;
+    nsresult rv = decoder->GetMaxLength(aNameData, aByteLen, &destLength);
+    if (NS_FAILED(rv)) {
+        NS_WARNING("decoder->GetMaxLength failed, invalid font name?");
+        return false;
+    }
+
+    // make space for the converted string
+    aName.SetLength(destLength);
+    rv = decoder->Convert(aNameData, &aByteLen,
+                          aName.BeginWriting(), &destLength);
+    if (NS_FAILED(rv)) {
+        NS_WARNING("decoder->Convert failed, invalid font name?");
+        return false;
+    }
+    aName.Truncate(destLength); // set the actual length
+
+    return true;
 }
 
 nsresult

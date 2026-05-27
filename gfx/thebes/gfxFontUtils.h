b3d160f81d9527c28c662a7e79721d8f4050f67c
K4sum1 <k4sum1@eclipse.cx>
2026-05-26 18:57:54 -0700

    Revert "Bug 1423742 - Stop using Encoding::ForName in gfxFontUtils. r=jfkthame"
    This reverts commit f6959223e5a6627a309981f66b19a2ac145891d6.

--- gfx/thebes/gfxFontUtils.h
+++ gfx/thebes/gfxFontUtils.h
@@ -11,7 +11,6 @@
 #include "nsComponentManagerUtils.h"
 #include "nsTArray.h"
 #include "mozilla/Likely.h"
-#include "mozilla/Encoding.h"
 #include "mozilla/EndianUtils.h"
 #include "mozilla/MemoryReporting.h"
 #include "mozilla/UniquePtr.h"
@@ -1001,25 +1000,26 @@ class gfxFontUtils {
                             uint32_t aNameID, int32_t aLangID,
                             int32_t aPlatformID, nsTArray<nsString>& aNames);
 
-  // convert opentype name-table platform/encoding/language values to an
-  // Encoding object we can use to convert the name data to unicode
-  static const mozilla::Encoding* GetCharsetForFontName(uint16_t aPlatform,
-                                                        uint16_t aScript,
-                                                        uint16_t aLanguage);
+  // convert opentype name-table platform/encoding/language values to a
+  // charset name we can use to convert the name data to unicode, or ""
+  // if data is UTF16BE
+  static const char* GetCharsetForFontName(uint16_t aPlatform,
+                                           uint16_t aScript,
+                                           uint16_t aLanguage);
 
   struct MacFontNameCharsetMapping {
-    uint16_t mScript;
+    uint16_t mEncoding;
     uint16_t mLanguage;
-    const mozilla::Encoding* mEncoding;
+    const char *mCharsetName;
 
     bool operator<(const MacFontNameCharsetMapping& rhs) const {
-      return (mScript < rhs.mScript) ||
-             ((mScript == rhs.mScript) && (mLanguage < rhs.mLanguage));
+      return (mEncoding < rhs.mEncoding) ||
+             ((mEncoding == rhs.mEncoding) && (mLanguage < rhs.mLanguage));
     }
   };
   static const MacFontNameCharsetMapping gMacFontNameCharsets[];
-  static const mozilla::Encoding* gISOFontNameCharsets[];
-  static const mozilla::Encoding* gMSFontNameCharsets[];
+  static const char* gISOFontNameCharsets[];
+  static const char* gMSFontNameCharsets[];
 };
 
 #endif /* GFX_FONT_UTILS_H */

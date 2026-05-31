6bc0878e195b466bc032a0ebcc9507325349aa8b
K4sum1 <k4sum1@eclipse.cx>
2026-05-31 03:34:05 -0700

    Revert "Bug 1071816 - Add a pref to fallback to UTF-8 for files from file: URLs. r=hsivonen"
    This reverts commit 0eee3482dc1c34c78f722d08955f70b4d71e90a9.

--- dom/encoding/FallbackEncoding.h
+++ dom/encoding/FallbackEncoding.h
@@ -27,11 +27,6 @@ public:
   static bool sGuessFallbackFromTopLevelDomain;
 
   /**
-   * Whether UTF-8 should be used for file URLs.
-   */
-  static bool sFallbackToUTF8ForFile;
-
-  /**
    * Gets the locale-dependent fallback encoding for legacy HTML and plain
    * text content.
    *

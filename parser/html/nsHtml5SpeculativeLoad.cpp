80fae5850c5fa264c7c53ebf94e5826ec8f29bd7
K4sum1 <k4sum1@eclipse.cx>
2026-05-31 03:34:18 -0700

    Revert "Bug 1424468 - Stop using Encoding::ForName in HTML parser. r=hsivonen"
    This reverts commit 082293ce3e19f321cb6180c6da4dd51c53c6a00d.

--- parser/html/nsHtml5SpeculativeLoad.cpp
+++ parser/html/nsHtml5SpeculativeLoad.cpp
@@ -17,7 +17,6 @@ nsHtml5SpeculativeLoad::nsHtml5SpeculativeLoad()
   mIsDefer(false)
 {
   MOZ_COUNT_CTOR(nsHtml5SpeculativeLoad);
-  new(&mCharsetOrSrcset) nsString;
 }
 
 nsHtml5SpeculativeLoad::~nsHtml5SpeculativeLoad()
@@ -25,9 +24,6 @@ nsHtml5SpeculativeLoad::~nsHtml5SpeculativeLoad()
   MOZ_COUNT_DTOR(nsHtml5SpeculativeLoad);
   NS_ASSERTION(mOpCode != eSpeculativeLoadUninitialized,
                "Uninitialized speculative load.");
-  if (mOpCode != eSpeculativeLoadSetDocumentCharset) {
-    mCharsetOrSrcset.~nsString();
-  }
 }
 
 void
@@ -79,10 +75,12 @@ nsHtml5SpeculativeLoad::Perform(nsHtml5TreeOpExecutor* aExecutor)
       aExecutor->ProcessOfflineManifest(mUrlOrSizes);
       break;
     case eSpeculativeLoadSetDocumentCharset: {
+        nsAutoCString narrowName;
+        CopyUTF16toUTF8(mCharsetOrSrcset, narrowName);
         NS_ASSERTION(mTypeOrCharsetSourceOrDocumentModeOrMetaCSPOrSizesOrIntegrity.Length() == 1,
             "Unexpected charset source string");
         int32_t intSource = (int32_t)mTypeOrCharsetSourceOrDocumentModeOrMetaCSPOrSizesOrIntegrity.First();
-        aExecutor->SetDocumentCharsetAndSource(WrapNotNull(mEncoding),
+        aExecutor->SetDocumentCharsetAndSource(Encoding::ForName(narrowName),
                                                intSource);
       }
       break;

220e2332d86338cef0cd6227e753d9fd4c832c13
K4sum1 <k4sum1@eclipse.cx>
2026-05-31 05:29:53 -0700

    static/unstatic whatever workarounds to improve later
    Update URLSearchParams.cpp

--- dom/url/URLSearchParams.cpp
+++ dom/url/URLSearchParams.cpp
@@ -98,7 +98,7 @@ URLParams::Delete(const nsAString& aName)
   }
 }
 
-/* static */ void
+void
 URLParams::ConvertString(const nsACString& aInput, nsAString& aOutput)
 {
   aOutput.Truncate();
@@ -136,7 +136,7 @@ URLParams::ConvertString(const nsACString& aInput, nsAString& aOutput)
   }
 }
 
-/* static */ void
+void
 URLParams::DecodeString(const nsACString& aInput, nsAString& aOutput)
 {
   nsACString::const_iterator start, end;
@@ -193,7 +193,7 @@ URLParams::DecodeString(const nsACString& aInput, nsAString& aOutput)
   ConvertString(unescaped, aOutput);
 }
 
-/* static */ bool
+bool
 URLParams::Parse(const nsACString& aInput, ForEachIterator& aIterator)
 {
   nsACString::const_iterator start, end;
@@ -284,7 +284,8 @@ URLParams::Extract(const nsACString& aInput,
 {
   aValue.SetIsVoid(true);
   ExtractURLParam iterator(aName, aValue);
-  return !URLParams::Parse(aInput, iterator);
+  mozilla::dom::URLParams params;
+  return params.Parse(aInput, iterator);
 }
 
 class MOZ_STACK_CLASS PopulateIterator final

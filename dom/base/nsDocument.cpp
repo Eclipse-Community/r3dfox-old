f9d65e6e922cbe4c47a5573a317d5a9784340091
K4sum1 <k4sum1@eclipse.cx>
2026-06-01 11:12:04 -0700

    Revert "Reapply "Bug 1354989 - Avoid pivoting via UTF-16 when loading CSS in the Stylo mode. r=jdm,SimonSapin""
    This reverts commit abd163f52c5910a5d6d72fcc5490daf06efa9d90.
    
    Update nsTreeSanitizer.cpp
    
    Revert "Kludge remove Servo code because I can't be bothered to convert it"
    
    This reverts commit 343bcc0e4059a3d610e1e54a780e2c2fc7447a49.
    
    Kludge remove Servo code because I can't be bothered to convert it
    
    Add to Revert "Reapply "Bug 1354989 - Avoid pivoting via UTF-16 when loading CSS in the Stylo mode. r=jdm,SimonSapin""
    
    Update nsDocument.cpp

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -9270,7 +9270,7 @@ NS_IMPL_ISUPPORTS(StubCSSLoaderObserver, nsICSSLoaderObserver)
 
 void
 nsIDocument::PreloadStyle(nsIURI* uri,
-                          const Encoding* aEncoding,
+                          const nsAString& charset,
                           const nsAString& aCrossOriginAttr,
                           const enum ReferrerPolicy aReferrerPolicy,
                           const nsAString& aIntegrity)
@@ -9282,7 +9282,7 @@ nsIDocument::PreloadStyle(nsIURI* uri,
   CSSLoader()->LoadSheet(uri,
                          true,
                          NodePrincipal(),
-                         aEncoding,
+                         NS_LossyConvertUTF16toASCII(charset),
                          obs,
                          Element::StringToCORSMode(aCrossOriginAttr),
                          aReferrerPolicy,

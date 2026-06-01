4b8f6076c6068fa08b172aad81f3e476f8e900b6
K4sum1 <k4sum1@eclipse.cx>
2026-06-01 11:12:04 -0700

    Revert "Reapply "Bug 1354989 - Avoid pivoting via UTF-16 when loading CSS in the Stylo mode. r=jdm,SimonSapin""
    This reverts commit abd163f52c5910a5d6d72fcc5490daf06efa9d90.
    
    Update nsTreeSanitizer.cpp
    
    Revert "Kludge remove Servo code because I can't be bothered to convert it"
    
    This reverts commit 343bcc0e4059a3d610e1e54a780e2c2fc7447a49.
    
    Kludge remove Servo code because I can't be bothered to convert it
    
    Add to Revert "Reapply "Bug 1354989 - Avoid pivoting via UTF-16 when loading CSS in the Stylo mode. r=jdm,SimonSapin""
    
    Update nsDocument.cpp

--- dom/xbl/nsXBLResourceLoader.cpp
+++ dom/xbl/nsXBLResourceLoader.cpp
@@ -157,7 +157,7 @@ nsXBLResourceLoader::LoadResources(nsIContent* aBoundElement)
       }
       else
       {
-        rv = cssLoader->LoadSheet(url, false, docPrincipal, nullptr, this);
+        rv = cssLoader->LoadSheet(url, false, docPrincipal, EmptyCString(), this);
         if (NS_SUCCEEDED(rv))
           ++mPendingSheets;
       }

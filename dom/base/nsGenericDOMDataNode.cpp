0b5ddb01972833561339539bc4765f3d7428a2f7
K4sum1 <k4sum1@eclipse.cx>
2026-06-01 13:22:59 -0700

    Revert "Bug 1431025 - Use encoding_rs::mem::is_utf16_bidi() as the implementation of HasRTLChars(). r=jfkthame"
    This reverts commit 5b05e804a9ad73d529f1265d5380b44ae3402368.

--- dom/base/nsGenericDOMDataNode.cpp
+++ dom/base/nsGenericDOMDataNode.cpp
@@ -349,7 +349,7 @@ nsGenericDOMDataNode::SetTextInternal(uint32_t aOffset, uint32_t aCount,
     if (aLength) {
       to.Append(aBuffer, aLength);
       if (!bidi && (!document || !document->GetBidiEnabled())) {
-        bidi = HasRTLChars(MakeSpan(aBuffer, aLength));
+        bidi = HasRTLChars(aBuffer, aLength);
       }
     }
     if (endOffset != textLength) {

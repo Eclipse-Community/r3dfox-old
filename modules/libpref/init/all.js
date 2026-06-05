d4c91d9df27cb17b4e977eb0eab862b90a6f4cb5
K4sum1 <k4sum1@eclipse.cx>
2026-06-05 05:04:20 -0700

    Disable some stupid shit

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -30,7 +30,7 @@ pref("general.useragent.site_specific_overrides", true);
 
 pref("general.config.obscure_value", 13); // for MCD .cfg files
 
-pref("general.warnOnAboutConfig", true);
+pref("general.warnOnAboutConfig", false);
 
 // maximum number of dated backups to keep at any time
 pref("browser.bookmarks.max_backups",       5);

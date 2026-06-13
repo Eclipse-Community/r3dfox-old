6b0bf839b89120211c40c64739c224db5e502b20
the_r3dacted <k4sum1@eclipse.cx>
2026-06-13 12:49:07 -0700

    Remove hardcoded MOZ_APP_BASENAME and MOZ_APP_VENDOR
    Specify this in branding instead, no reason not to.

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -3,8 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_BASENAME=eXPlorer
-MOZ_APP_VENDOR="Eclipse Community"
 MOZ_UPDATER=1
 MOZ_PHOENIX=1
 
@@ -34,12 +32,9 @@ else
     MOZ_APP_VERSION=`cat ${_topsrcdir}/$MOZ_BUILD_APP/config/version.txt`
     MOZ_APP_VERSION_DISPLAY=`cat ${_topsrcdir}/$MOZ_BUILD_APP/config/version_display.txt`
 fi
-# MOZ_APP_DISPLAYNAME will be set by branding/configure.sh
+# MOZ_APP_*NAME will be set by branding/configure.sh
 # MOZ_BRANDING_DIRECTORY is the default branding directory used when none is
 # specified. It should never point to the "official" branding directory.
-# For mozilla-beta, mozilla-release, or mozilla-central repositories, use
-# "unofficial" branding.
-# For the mozilla-aurora repository, use "aurora".
 MOZ_BRANDING_DIRECTORY=browser/branding/unofficial
 MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/official
 MOZ_APP_ID={ec8030f7-c20a-464f-9b0e-13a3a9e97384}

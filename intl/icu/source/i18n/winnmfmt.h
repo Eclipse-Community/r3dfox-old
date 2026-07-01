45734374343a77eb8a935db85d4d918801cae2a0
K4sum1 <k4sum1@eclipse.cx>
2026-07-01 14:49:55 -0700

    partial icu fix while I figure out what to do about putil.cpp and locmap.cpp

--- intl/icu/source/i18n/winnmfmt.h
+++ intl/icu/source/i18n/winnmfmt.h
@@ -155,7 +155,6 @@ private:
     FormatInfo *fFormatInfo;
     UBool fFractionDigitsSet;
 
-    UnicodeString* fWindowsLocaleName; // Stores the equivalent Windows locale name.
 };
 
 U_NAMESPACE_END

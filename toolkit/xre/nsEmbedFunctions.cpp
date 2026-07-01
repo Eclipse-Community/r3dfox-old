7d49f8cdb4900721f1779c772f5cb48d0c4a0c5b
K4sum1 <k4sum1@eclipse.cx>
2026-07-01 15:19:09 -0700

    Revert "Bug 1348177 - Part 2. Remove GetProcAddress for shell32 to use Vista+/7+ API (tookit part). r=jimm"
    This reverts commit 8a66aa54a4dea83fe5fab12ba81a4fc2b835acdb.

--- toolkit/xre/nsEmbedFunctions.cpp
+++ toolkit/xre/nsEmbedFunctions.cpp
@@ -24,7 +24,6 @@
 
 #ifdef XP_WIN
 #include <process.h>
-#include <shobjidl.h>
 #include "mozilla/ipc/WindowsMessageLoop.h"
 #endif
 
@@ -142,6 +141,10 @@ using mozilla::ipc::XPCShellEnvironment;
 using mozilla::startup::sChildProcessType;
 
 static NS_DEFINE_CID(kAppShellCID, NS_APPSHELL_CID);
+
+#ifdef XP_WIN
+static const wchar_t kShellLibraryName[] =  L"shell32.dll";
+#endif
 
 nsresult XRE_LockProfileDirectory(nsIFile* aDirectory,
                                   nsISupports** aLockObject) {
@@ -279,10 +282,26 @@ XRE_SetRemoteExceptionHandler(const char* aPipe /*= 0*/)
 
 #if defined(XP_WIN)
 void SetTaskbarGroupId(const nsString& aId) {
-  if (FAILED(SetCurrentProcessExplicitAppUserModelID(aId.get()))) {
-    NS_WARNING(
-        "SetCurrentProcessExplicitAppUserModelID failed for child process.");
+  typedef HRESULT (WINAPI * SetCurrentProcessExplicitAppUserModelIDPtr)(PCWSTR AppID);
+
+  SetCurrentProcessExplicitAppUserModelIDPtr funcAppUserModelID = nullptr;
+
+  HMODULE hDLL = ::LoadLibraryW(kShellLibraryName);
+
+  funcAppUserModelID = (SetCurrentProcessExplicitAppUserModelIDPtr)
+                        GetProcAddress(hDLL, "SetCurrentProcessExplicitAppUserModelID");
+
+  if (!funcAppUserModelID) {
+    ::FreeLibrary(hDLL);
+    return;
   }
+
+  if (FAILED(funcAppUserModelID(aId.get()))) {
+    NS_WARNING("SetCurrentProcessExplicitAppUserModelID failed for child process.");
+  }
+
+  if (hDLL)
+    ::FreeLibrary(hDLL);
 }
 #endif
 

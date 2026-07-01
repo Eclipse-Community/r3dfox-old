091e5fc89540d92dd2c8918f2b32b30ff22a2365
K4sum1 <k4sum1@eclipse.cx>
2026-07-01 15:47:29 -0700

    Revert "Remove the Direct3D9 compositor. (bug 1318558 part 2, r=mattwoodrow)"
    This reverts commit 315fd708c897732891a563b9338becb481a0b8a5.

--- gfx/layers/Compositor.h
+++ gfx/layers/Compositor.h
@@ -131,6 +131,7 @@ class CompositingRenderTarget;
 class CompositorBridgeParent;
 class LayerManagerComposite;
 class CompositorOGL;
+class CompositorD3D9;
 class CompositorD3D11;
 class BasicCompositor;
 class TextureReadLock;
@@ -427,6 +428,7 @@ class Compositor : public TextureSourceProvider {
   virtual LayersBackend GetBackendType() const = 0;
 
   virtual CompositorOGL* AsCompositorOGL() { return nullptr; }
+  virtual CompositorD3D9* AsCompositorD3D9() { return nullptr; }
   virtual CompositorD3D11* AsCompositorD3D11() { return nullptr; }
   virtual BasicCompositor* AsBasicCompositor() { return nullptr; }
 

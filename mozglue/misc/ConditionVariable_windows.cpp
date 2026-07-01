2998b9b27ced2fb7160dec40a0734d7cd0ff340f
K4sum1 <k4sum1@eclipse.cx>
2026-07-01 13:52:57 -0700

    Revert "Bug 1364624 - Switch from CRITICALSECTION to SRWLOCK. r=froydnj"
    This reverts commit 2f24c4f229039dbbe2d8a93a2b760a3a06cc1d0e.

--- mozglue/misc/ConditionVariable_windows.cpp
+++ mozglue/misc/ConditionVariable_windows.cpp
@@ -46,15 +46,14 @@ void mozilla::detail::ConditionVariableImpl::notify_all() {
 }
 
 void mozilla::detail::ConditionVariableImpl::wait(MutexImpl& lock) {
-  SRWLOCK* srwlock = &lock.platformData()->lock;
-  bool r =
-      SleepConditionVariableSRW(&platformData()->cv_, srwlock, INFINITE, 0);
+  CRITICAL_SECTION* cs = &lock.platformData()->criticalSection;
+  bool r = SleepConditionVariableCS(&platformData()->cv_, cs, INFINITE);
   MOZ_RELEASE_ASSERT(r);
 }
 
 mozilla::detail::CVStatus mozilla::detail::ConditionVariableImpl::wait_for(
     MutexImpl& lock, const mozilla::TimeDuration& rel_time) {
-  SRWLOCK* srwlock = &lock.platformData()->lock;
+  CRITICAL_SECTION* cs = &lock.platformData()->criticalSection;
 
   // Note that DWORD is unsigned, so we have to be careful to clamp at 0.
   // If rel_time is Forever, then ToMilliseconds is +inf, which evaluates as
@@ -64,7 +63,7 @@ mozilla::detail::CVStatus mozilla::detail::ConditionVariableImpl::wait_for(
                    ? 0
                    : msecd > UINT32_MAX ? INFINITE : static_cast<DWORD>(msecd);
 
-  BOOL r = SleepConditionVariableSRW(&platformData()->cv_, srwlock, msec, 0);
+  BOOL r = SleepConditionVariableCS(&platformData()->cv_, cs, msec);
   if (r) return CVStatus::NoTimeout;
   MOZ_RELEASE_ASSERT(GetLastError() == ERROR_TIMEOUT);
   return CVStatus::Timeout;
--- mozglue/misc/Mutex_windows.cpp
+++ mozglue/misc/Mutex_windows.cpp
@@ -13,17 +13,39 @@
 #include "MutexPlatformData_windows.h"
 
 mozilla::detail::MutexImpl::MutexImpl() {
-  InitializeSRWLock(&platformData()->lock);
+  // This number was adopted from NSPR.
+  const static DWORD LockSpinCount = 1500;
+
+#if defined(RELEASE_OR_BETA)
+  // Vista and later automatically allocate and subsequently leak a debug info
+  // object for each critical section that we allocate unless we tell the
+  // system not to do that.
+  DWORD flags = CRITICAL_SECTION_NO_DEBUG_INFO;
+#else
+  DWORD flags = 0;
+#endif // defined(RELEASE_OR_BETA)
+
+  BOOL r = InitializeCriticalSectionEx(&platformData()->criticalSection,
+                                       LockSpinCount, flags);
+  MOZ_RELEASE_ASSERT(r);
 }
 
-mozilla::detail::MutexImpl::~MutexImpl() {}
+mozilla::detail::MutexImpl::~MutexImpl() {
+  if (!platformData_)
+    return;
+  DeleteCriticalSection(&platformData()->criticalSection);
+}
 
 void mozilla::detail::MutexImpl::lock() {
-  AcquireSRWLockExclusive(&platformData()->lock);
+  if (!platformData_)
+    return;
+  EnterCriticalSection(&platformData()->criticalSection);
 }
 
 void mozilla::detail::MutexImpl::unlock() {
-  ReleaseSRWLockExclusive(&platformData()->lock);
+  if (!platformData_)
+    return;
+  LeaveCriticalSection(&platformData()->criticalSection);
 }
 
 mozilla::detail::MutexImpl::PlatformData*

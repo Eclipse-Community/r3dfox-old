a4aabd3be6ecb71811d19986a86bff46b408a8fc
K4sum1 <k4sum1@eclipse.cx>
2026-07-02 14:02:27 -0700

    Revert "webrtc task_queue_win.cc InitOnceExecuteOnce workaround"
    This reverts commit d3df183c79f515c26511f831486f03958b1be5d3.

--- media/webrtc/trunk/webrtc/base/task_queue_win.cc
+++ media/webrtc/trunk/webrtc/base/task_queue_win.cc
@@ -11,11 +11,6 @@
 #include "webrtc/base/task_queue.h"
 
 #include <string.h>
-#include <winternl.h>
-#include <ntstatus.h>
-
-extern "C" DECLSPEC_IMPORT NTSTATUS WINAPI NtWaitForKeyedEvent(HANDLE, const void*, BOOLEAN, const LARGE_INTEGER*);
-extern "C" DECLSPEC_IMPORT NTSTATUS WINAPI NtReleaseKeyedEvent(HANDLE, const void*, BOOLEAN, const LARGE_INTEGER*);
 
 #include "webrtc/base/checks.h"
 #include "webrtc/base/logging.h"
@@ -32,82 +27,9 @@ BOOL CALLBACK InitializeTls(PINIT_ONCE init_once, void* param, void** context) {
   return TRUE;
 }
 
-  DWORD sRunOnceComplete(INIT_ONCE* once, ULONG flags, void* context) {
-    if ((ULONG_PTR)context & 3) return STATUS_INVALID_PARAMETER;
-
-    if (flags & RTL_RUN_ONCE_INIT_FAILED) {
-      if (context) return STATUS_INVALID_PARAMETER;
-      if (flags & RTL_RUN_ONCE_ASYNC) return STATUS_INVALID_PARAMETER;
-    }
-    else context = (void *)((ULONG_PTR)context | 2);
-
-    for (;;) {
-      ULONG_PTR val = (ULONG_PTR)once->Ptr;
-      switch (val & 3) {
-        case 1:  /* in progress */
-          if (::InterlockedCompareExchangePointer( &once->Ptr, context, (void *)val ) != (void *)val) break;
-          val &= ~3;
-          while (val) {
-            ULONG_PTR next = *(ULONG_PTR *)val;
-            NtReleaseKeyedEvent( 0, (void *)val, FALSE, NULL );
-            val = next;
-            }
-            return STATUS_SUCCESS;
-
-        case 3:  /* in progress, async */
-          if (!(flags & RTL_RUN_ONCE_ASYNC)) return STATUS_INVALID_PARAMETER;
-          if (::InterlockedCompareExchangePointer( &once->Ptr, context, (void *)val ) != (void *)val) break;
-          return STATUS_SUCCESS;
-
-        default:
-          return STATUS_UNSUCCESSFUL;
-      }
-    }
-  }
-
-
-  DWORD sInitOnceExecuteOnce(INIT_ONCE* once, PINIT_ONCE_FN func, void* param, void ** context) {
-    DWORD ret = 0;
-    for (;;) {
-for_begin:
-      ULONG_PTR next, val = (ULONG_PTR)&once->Ptr;
-
-      switch (val & 3) {
-        case 0:  /* first time */
-          if (!::InterlockedCompareExchangePointer(&once->Ptr, (void *)1, 0 ))
-            ret = STATUS_PENDING;
-          break;
-
-        case 1:  /* in progress, wait */
-          next = val & ~3;
-          if (::InterlockedCompareExchangePointer(&once->Ptr, (void *)((ULONG_PTR)&next | 1), (void *)val ) == (void *)val)
-            NtWaitForKeyedEvent( 0, &next, FALSE, NULL );
-          goto for_begin;
-
-        case 2:  /* done */
-          ret = STATUS_SUCCESS;
-          break;
-
-        case 3:  /* in progress, async */
-          ret = STATUS_INVALID_PARAMETER;
-          break;
-       }
-      break;
-    }
-
-  if (ret != STATUS_PENDING) return ret;
-
-  if (!func(once, param, context)) {
-    sRunOnceComplete(once, RTL_RUN_ONCE_INIT_FAILED, NULL);
-    return STATUS_UNSUCCESSFUL;
-  }
-
-  return sRunOnceComplete(once, 0, context ? *context : NULL);
-  }
-
 DWORD GetQueuePtrTls() {
   static INIT_ONCE init_once = INIT_ONCE_STATIC_INIT;
-  sInitOnceExecuteOnce(&init_once, InitializeTls, nullptr, nullptr);
+  InitOnceExecuteOnce(&init_once, InitializeTls, nullptr, nullptr);
   return g_queue_ptr_tls;
 }
 

8ba1b9649bd27d13e056da6db851db22b471f0a4
K4sum1 <k4sum1@eclipse.cx>
2026-07-06 10:57:14 -0700

    Mypal68 InitOnceExecOnceXP

--- media/webrtc/trunk/webrtc/base/task_queue_win.cc
+++ media/webrtc/trunk/webrtc/base/task_queue_win.cc
@@ -15,6 +15,8 @@
 #include "webrtc/base/checks.h"
 #include "webrtc/base/logging.h"
 
+#include "InitOnceExecOnceXP.h"
+
 namespace rtc {
 namespace {
 #define WM_RUN_TASK WM_USER + 1
@@ -29,7 +31,7 @@ BOOL CALLBACK InitializeTls(PINIT_ONCE init_once, void* param, void** context) {
 
 DWORD GetQueuePtrTls() {
   static INIT_ONCE init_once = INIT_ONCE_STATIC_INIT;
-  InitOnceExecuteOnce(&init_once, InitializeTls, nullptr, nullptr);
+  WinxpStuff::InitOnceExecOnceXP(&init_once, InitializeTls, nullptr, nullptr);
   return g_queue_ptr_tls;
 }
 

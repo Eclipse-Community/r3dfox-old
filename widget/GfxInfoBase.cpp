4ff2da1c0cb437b5692e0f0e2cfb7cf09ec7c42d
K4sum1 <k4sum1@eclipse.cx>
2026-07-06 18:19:53 -0700

    Revert "Bug 1349476 - Remove LayersBackend::LAYERS_D3D9 type r=mattwoodrow"
    This reverts commit b98245c5fc0e45c01e757420f9f1ed0ce668ea94.

--- widget/GfxInfoBase.cpp
+++ widget/GfxInfoBase.cpp
@@ -1195,6 +1195,8 @@ GetLayersBackendName(layers::LayersBackend aBackend)
       return "none";
     case layers::LayersBackend::LAYERS_OPENGL:
       return "opengl";
+    case layers::LayersBackend::LAYERS_D3D9:
+      return "d3d9";
     case layers::LayersBackend::LAYERS_D3D11:
       return "d3d11";
     case layers::LayersBackend::LAYERS_CLIENT:

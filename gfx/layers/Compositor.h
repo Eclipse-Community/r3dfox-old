a4940d5fd5d128167e13c82ca4aed29a74686c12
K4sum1 <k4sum1@eclipse.cx>
2026-07-06 18:20:04 -0700

    Revert "Remove the Direct3D9 compositor. (bug 1318558 part 2, r=mattwoodrow)"
    This reverts commit 990418f338a30fc5661f71127ce9b7bcf359f627.

--- gfx/layers/Compositor.h
+++ gfx/layers/Compositor.h
@@ -131,6 +131,7 @@ class CompositingRenderTarget;
 class CompositorBridgeParent;
 class LayerManagerComposite;
 class CompositorOGL;
+class CompositorD3D9;
 class CompositorD3D11;
 class BasicCompositor;
 class TextureReadLock;
@@ -456,6 +457,7 @@ public:
   virtual LayersBackend GetBackendType() const = 0;
 
   virtual CompositorOGL* AsCompositorOGL() { return nullptr; }
+  virtual CompositorD3D9* AsCompositorD3D9() { return nullptr; }
   virtual CompositorD3D11* AsCompositorD3D11() { return nullptr; }
   virtual BasicCompositor* AsBasicCompositor() { return nullptr; }
 
--- gfx/layers/composite/TextureHost.h
+++ gfx/layers/composite/TextureHost.h
@@ -60,6 +60,7 @@ class MacIOSurfaceTextureHostOGL;
 class TextureHostOGL;
 class TextureReadLock;
 class TextureSourceOGL;
+class TextureSourceD3D9;
 class TextureSourceD3D11;
 class TextureSourceBasic;
 class DataTextureSource;
@@ -131,6 +132,7 @@ public:
     gfxCriticalNote << "Failed to cast " << Name() << " into a TextureSourceOGL";
     return nullptr;
   }
+  virtual TextureSourceD3D9* AsSourceD3D9() { return nullptr; }
   virtual TextureSourceD3D11* AsSourceD3D11() { return nullptr; }
   virtual TextureSourceBasic* AsSourceBasic() { return nullptr; }
   /**

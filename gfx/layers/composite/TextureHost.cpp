32de5469790c09d19933526c0c3346f59f42790d
K4sum1 <k4sum1@eclipse.cx>
2026-07-06 19:17:59 -0700

    Other fixs that might just be me being rarted

--- gfx/layers/composite/TextureHost.cpp
+++ gfx/layers/composite/TextureHost.cpp
@@ -181,6 +181,7 @@ already_AddRefed<TextureHost> CreateTextureHostD3D11(const SurfaceDescriptor& aD
 // implemented in TextureD3D9.cpp
 already_AddRefed<TextureHost> CreateTextureHostD3D9(const SurfaceDescriptor& aDesc,
                                                 ISurfaceAllocator* aDeallocator,
+                                                LayersBackend aBackend,
                                                 TextureFlags aFlags);
 
 already_AddRefed<TextureHost>
@@ -218,14 +219,14 @@ TextureHost::Create(const SurfaceDescriptor& aDesc,
 
 #ifdef XP_WIN
     case SurfaceDescriptor::TSurfaceDescriptorD3D9:
-      return CreateTextureHostD3D9(aDesc, aDeallocator, aFlags);
+      return CreateTextureHostD3D9(aDesc, aDeallocator, aBackend, aFlags);
 
     case SurfaceDescriptor::TSurfaceDescriptorD3D10:
     case SurfaceDescriptor::TSurfaceDescriptorDXGIYCbCr:
       if (aBackend == LayersBackend::LAYERS_D3D9) {
-        return CreateTextureHostD3D9(aDesc, aDeallocator, aFlags);
+        return CreateTextureHostD3D9(aDesc, aDeallocator, aBackend, aFlags);
       } else {
-        return CreateTextureHostD3D11(aDesc, aDeallocator, aFlags);
+        return CreateTextureHostD3D11(aDesc, aDeallocator, aBackend, aFlags);
       }
 #endif
     default:

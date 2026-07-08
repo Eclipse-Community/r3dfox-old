b2a696a03c33ba5be4282ab6485e28ab36ab9df1
K4sum1 <k4sum1@eclipse.cx>
2026-07-08 14:25:12 -0700

    Revert "Bug 1423806 - Enable IMFYCbCrImage with WebRender r=mattwoodrow"
    This reverts commit 2063225117e3a1396a1cf8c7408e844a107cddaf.

--- dom/media/platforms/wmf/WMFVideoMFTManager.cpp
+++ dom/media/platforms/wmf/WMFVideoMFTManager.cpp
@@ -961,7 +961,8 @@ WMFVideoMFTManager::CreateBasicVideoFrame(IMFSample* aSample,
   gfx::IntRect pictureRegion =
       mVideoInfo.ScaledImageRect(videoWidth, videoHeight);
 
-  if (!mKnowsCompositor || !mKnowsCompositor->SupportsD3D11() || !mIMFUsable) {
+  LayersBackend backend = GetCompositorBackendType(mKnowsCompositor);
+  if (backend != LayersBackend::LAYERS_D3D11 || !mIMFUsable) {
     RefPtr<VideoData> v = VideoData::CreateAndCopyData(
         mVideoInfo, mImageContainer, aStreamOffset, pts, duration, b, false,
         TimeUnit::FromMicroseconds(-1), pictureRegion);

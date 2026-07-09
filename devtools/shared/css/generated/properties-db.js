446dcc21522ee42dff3c4e04a6a4b73f495f6516
K4sum1 <k4sum1@eclipse.cx>
2026-07-09 02:13:00 -0700

    Revert "Bug 1417200: Make -moz-border-colors chrome only. r=xidorn"
    This reverts commit 13a91fa9f670363cbc870046930e556b1d937a98.

--- devtools/shared/css/generated/properties-db.js
+++ devtools/shared/css/generated/properties-db.js
@@ -351,6 +351,20 @@ exports.CSS_PROPERTIES = {
       "url"
     ]
   },
+  "-moz-border-bottom-colors": {
+    "isInherited": false,
+    "subproperties": [
+      "-moz-border-bottom-colors"
+    ],
+    "supports": [
+      2
+    ],
+    "values": [
+      "inherit",
+      "initial",
+      "unset"
+    ]
+  },
   "-moz-border-end": {
     "isInherited": false,
     "subproperties": [
@@ -490,6 +504,34 @@ exports.CSS_PROPERTIES = {
       "url"
     ]
   },
+  "-moz-border-left-colors": {
+    "isInherited": false,
+    "subproperties": [
+      "-moz-border-left-colors"
+    ],
+    "supports": [
+      2
+    ],
+    "values": [
+      "inherit",
+      "initial",
+      "unset"
+    ]
+  },
+  "-moz-border-right-colors": {
+    "isInherited": false,
+    "subproperties": [
+      "-moz-border-right-colors"
+    ],
+    "supports": [
+      2
+    ],
+    "values": [
+      "inherit",
+      "initial",
+      "unset"
+    ]
+  },
   "-moz-border-start": {
     "isInherited": false,
     "subproperties": [
@@ -586,6 +628,20 @@ exports.CSS_PROPERTIES = {
       "medium",
       "thick",
       "thin",
+      "unset"
+    ]
+  },
+  "-moz-border-top-colors": {
+    "isInherited": false,
+    "subproperties": [
+      "-moz-border-top-colors"
+    ],
+    "supports": [
+      2
+    ],
+    "values": [
+      "inherit",
+      "initial",
       "unset"
     ]
   },
@@ -3683,6 +3739,10 @@ exports.CSS_PROPERTIES = {
       "border-right-color",
       "border-bottom-color",
       "border-left-color",
+      "-moz-border-top-colors",
+      "-moz-border-right-colors",
+      "-moz-border-bottom-colors",
+      "-moz-border-left-colors",
       "border-image-source",
       "border-image-slice",
       "border-image-width",

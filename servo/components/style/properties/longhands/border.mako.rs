995274ffa67db61080701e915d51fadc0e09f818
K4sum1 <k4sum1@eclipse.cx>
2026-07-09 02:09:09 -0700

    Revert "servo: Merge #19446 - style: Make -moz-border-colors chrome only (from emilio:bye-moz-border-colors); r=xidorn"
    This reverts commit bde5c06616d4492533ebcaade6db56c9c5836b0e.

--- servo/components/style/properties/longhands/border.mako.rs
+++ servo/components/style/properties/longhands/border.mako.rs
@@ -89,7 +89,6 @@ ${helpers.gecko_keyword_conversion(
                        spec="Nonstandard (https://developer.mozilla.org/en-US/docs/Web/CSS/-moz-border-*-colors)"
                        products="gecko"
                        flags="APPLIES_TO_FIRST_LETTER"
-                       enabled_in="chrome"
                        ignored_when_colors_disabled="True">
         use std::fmt;
         use style_traits::ToCss;

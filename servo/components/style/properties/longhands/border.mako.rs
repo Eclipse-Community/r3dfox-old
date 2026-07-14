db4920f37e791785517b506efcb63ff32a814c91
K4sum1 <k4sum1@eclipse.cx>
2026-07-14 06:50:54 -0700

    rust code fixs

--- servo/components/style/properties/longhands/border.mako.rs
+++ servo/components/style/properties/longhands/border.mako.rs
@@ -92,8 +92,8 @@ ${helpers.gecko_keyword_conversion(
                        products="gecko"
                        flags="APPLIES_TO_FIRST_LETTER"
                        ignored_when_colors_disabled="True">
-        use std::fmt;
-        use style_traits::ToCss;
+        use std::fmt::{self, Write};
+        use style_traits::{CssWriter, ToCss};
         use values::specified::RGBAColor;
 
         pub mod computed_value {
@@ -109,7 +109,7 @@ ${helpers.gecko_keyword_conversion(
         }
 
         impl ToCss for computed_value::T {
-            fn to_css<W>(&self, dest: &mut W) -> fmt::Result where W: fmt::Write {
+            fn to_css<W>(&self, dest: &mut CssWriter<W>) -> fmt::Result where W: Write {
                 match self.0 {
                     None => return dest.write_str("none"),
                     Some(ref vec) => {
@@ -128,7 +128,7 @@ ${helpers.gecko_keyword_conversion(
         }
 
         impl ToCss for SpecifiedValue {
-            fn to_css<W>(&self, dest: &mut W) -> fmt::Result where W: fmt::Write {
+            fn to_css<W>(&self, dest: &mut CssWriter<W>) -> fmt::Result where W: Write {
                 match *self {
                     SpecifiedValue::None => return dest.write_str("none"),
                     SpecifiedValue::Colors(ref vec) => {

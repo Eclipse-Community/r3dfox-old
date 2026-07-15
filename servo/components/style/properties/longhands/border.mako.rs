8bdfca09ad3f1b2a5e9e294fbc243da539b4d762
K4sum1 <k4sum1@eclipse.cx>
2026-07-14 21:22:15 -0700

    Most rust code fixes (this feels like bloat why)

--- servo/components/style/properties/longhands/border.mako.rs
+++ servo/components/style/properties/longhands/border.mako.rs
@@ -96,11 +96,11 @@ ${helpers.gecko_keyword_conversion(
 
         pub mod computed_value {
             use cssparser::RGBA;
-            #[derive(Clone, Debug, MallocSizeOf, PartialEq)]
+            #[derive(Clone, Debug, MallocSizeOf, PartialEq, SpecifiedValueInfo)]
             pub struct T(pub Option<Vec<RGBA>>);
         }
 
-        #[derive(Clone, Debug, MallocSizeOf, PartialEq)]
+        #[derive(Clone, Debug, MallocSizeOf, PartialEq, SpecifiedValueInfo)]
         pub enum SpecifiedValue {
             None,
             Colors(Vec<RGBAColor>),
